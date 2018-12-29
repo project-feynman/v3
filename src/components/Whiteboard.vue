2bb0cd9f54d48627752b0f167aa86211236d95fb
Elton Lin <eltonlin1998@gmail.com>
2018-12-29 10:29:45 +0800

    whiteboard resizes and draws expectedly

--- src/components/Whiteboard.vue
+++ src/components/Whiteboard.vue
@@ -1,7 +1,8 @@
 <template>
+  <!-- http://www.ckollars.org/canvas-two-coordinate-scales.html#scaling -->
   <!-- https://zipso.net/a-simple-touchscreen-sketchpad-using-javascript-and-html5/ -->
   <div class="whiteboard">
-    <canvas id="myCanvas"></canvas>
+    <canvas id="myCanvas" width="1500" height="1500"></canvas>
   </div>
 </template>
 
@@ -10,7 +11,7 @@
   width: 100%;
   background-color: rgb(192, 230, 253);
   cursor: crosshair;
-  position: fixed;
+  /* position: fixed; */
 }
 </style>
 
@@ -30,90 +31,96 @@ export default {
     }
   },
   mounted() {
-    const myCanvas = document.getElementById('myCanvas')
-    const ctx = myCanvas.getContext('2d')
-    let isDown = false 
-    ctx.lineWidth = 3
-
-    // mouse event handlers 
-
-
-    // touch event handlers 
+    this.canvas = document.getElementById('myCanvas')
+    this.ctx = this.canvas.getContext('2d')
+    this.rescaleCanvas()
+    window.addEventListener('resize', this.rescaleCanvas, false)
+    this.initTouchEvents()
   },
   methods: {
+    rescaleCanvas() {
+      // console.log('INITIALLY')
+      // console.log('model width and height =', this.canvas.width, this.canvas.height)
+      // console.log('display width and height =', this.canvas.scrollWidth, this.canvas.scrollHeight)
+      this.canvas.width = this.canvas.scrollWidth
+      this.canvas.height = this.canvas.scrollHeight
+      // console.log('AFTERWARDS')
+      // console.log('model width and height =', this.canvas.width, this.canvas.height)
+      // console.log('display width and height =', this.canvas.scrollWidth, this.canvas.scrollHeight)
+    },
     initTouchEvents() {
-
+      this.canvas.addEventListener('touchstart', this.touchStart, false)
+      this.canvas.addEventListener('touchend',this.touchEnd, false)
+      this.canvas.addEventListener('touchmove', this.touchMove, false)
     },
     initMouseEvents() {
 
+    },
+    clearCanvas() {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    drawLine(x, y, size) {
+        // If lastX is not set, set lastX and lastY to the current position 
+        if (this.lastX == -1) {
+          this.lastX = x
+	        this.lastY = y
+        }
+        
+        // Select a fill style
+        this.ctx.strokeStyle = 'black'
+
+        // Set the line "cap" style to round, so lines at different angles can join into each other
+        this.ctx.lineCap = "round";
+        //ctx.lineJoin = "round";
+
+        // Draw a filled line
+        this.ctx.beginPath();
+
+        //console.log('last position =', this.lastX, this.lastY)
+        this.ctx.moveTo(this.lastX, this.lastY)
+        this.ctx.lineTo(x,y)
+        //console.log('new position =', x, y)
+
+        // Set the line thickness and draw the line
+        this.ctx.lineWidth = size
+        this.ctx.stroke()
+
+        this.ctx.closePath();
+
+        // Update the last position to reference the current position
+        this.lastX = x
+        this.lastY = y
+    },
+    touchStart(e) {
+      this.getTouchPos(e) 
+      this.drawLine(this.touchX, this.touchY, 3)
+    },
+    touchMove(e) {
+      this.getTouchPos(e)
+      this.drawLine(this.touchX, this.touchY, 3)
+      e.preventDefault()
+    },
+    touchEnd(e) {
+      this.lastX = -1
+      this.lastY = -1 
+    },
+    getTouchPos(e) {
+      // Get the touch position relative to the top-left of the canvas
+      // When we get the raw values of pageX and pageY below, they take into account the scrolling on the page
+      // but not the position relative to our target div. We'll adjust them using "target.offsetLeft" and
+      // "target.offsetTop" to get the correct values in relation to the top left of the canvas.
+      if (e.touches) {
+        if (e.touches.length == 1) { // Only deal with one finger
+          const touch = e.touches[0]; // Get the information for finger #1
+          this.touchX = touch.pageX - this.canvas.getBoundingClientRect().left
+          this.touchY = touch.pageY - this.canvas.getBoundingClientRect().top
+          // this.touchX = touch.pageX - touch.target.offsetLeft
+          // this.touchY = touch.pageY - touch.target.offsetTop
+        }
+      }
     }
   }
 }
-
-// <script type="text/javascript">
-
-
-
-// INITIALIZE VARIABLES 
-//     // Variables for referencing the canvas and 2dcanvas context
-//     var canvas,ctx;
-
-//     // Variables to keep track of the mouse position and left-button status 
-//     var mouseX,mouseY,mouseDown=0;
-
-//     // Variables to keep track of the touch position
-//     var touchX,touchY;
-
-//     // Keep track of the old/last position when drawing a line
-//     // We set it to -1 at the start to indicate that we don't have a good value for it yet
-//     var lastX,lastY=-1;
-
-// FUNCTIONS 
-//     // Draws a line between the specified position on the supplied canvas name
-//     // Parameters are: A canvas context, the x position, the y position, the size of the dot
-//     function drawLine(ctx,x,y,size) {
-
-//         // If lastX is not set, set lastX and lastY to the current position 
-//         if (lastX==-1) {
-//             lastX=x;
-// 	    lastY=y;
-//         }
-
-//         // Let's use black by setting RGB values to 0, and 255 alpha (completely opaque)
-//         r=0; g=0; b=0; a=255;
-
-//         // Select a fill style
-//         ctx.strokeStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
-
-//         // Set the line "cap" style to round, so lines at different angles can join into each other
-//         ctx.lineCap = "round";
-//         //ctx.lineJoin = "round";
-
-
-//         // Draw a filled line
-//         ctx.beginPath();
-
-// 	// First, move to the old (previous) position
-// 	ctx.moveTo(lastX,lastY);
-
-// 	// Now draw a line to the current touch/pointer position
-// 	ctx.lineTo(x,y);
-
-//         // Set the line thickness and draw the line
-//         ctx.lineWidth = size;
-//         ctx.stroke();
-
-//         ctx.closePath();
-
-// 	// Update the last position to reference the current position
-// 	lastX=x;
-// 	lastY=y;
-//     } 
-
-//     // Clear the canvas context using the canvas width and height
-//     function clearCanvas(canvas,ctx) {
-//         ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     }
 
 //     // Keep track of the mouse button being pressed and draw a dot at current location
 //     function sketchpad_mouseDown() {
@@ -156,16 +163,7 @@ export default {
 //         }
 //      }
 
-//     // Draw something when a touch start is detected
-//     function sketchpad_touchStart() {
-//         // Update the touch co-ordinates
-//         getTouchPos();
 
-//         drawLine(ctx,touchX,touchY,12);
-
-//         // Prevents an additional mousedown event being triggered
-//         event.preventDefault();
-//     }
 
 //     function sketchpad_touchEnd() {
 //         // Reset lastX and lastY to -1 to indicate that they are now invalid, since we have lifted the "pen"
@@ -185,22 +183,7 @@ export default {
 //         event.preventDefault();
 //     }
 
-//     // Get the touch position relative to the top-left of the canvas
-//     // When we get the raw values of pageX and pageY below, they take into account the scrolling on the page
-//     // but not the position relative to our target div. We'll adjust them using "target.offsetLeft" and
-//     // "target.offsetTop" to get the correct values in relation to the top left of the canvas.
-//     function getTouchPos(e) {
-//         if (!e)
-//             var e = event;
 
-//         if(e.touches) {
-//             if (e.touches.length == 1) { // Only deal with one finger
-//                 var touch = e.touches[0]; // Get the information for finger #1
-//                 touchX=touch.pageX-touch.target.offsetLeft;
-//                 touchY=touch.pageY-touch.target.offsetTop;
-//             }
-//         }
-//     }
 
 
 //     // Set-up the canvas and add our event handlers after the page has loaded
