4b70ed86d11ea0165093d5dc6f6e774e046551d6
Elton Lin <eltonlin1998@gmail.com>
2019-01-04 20:55:30 +0800

    use dividers more elegantly on the left drawer

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,8 +1,26 @@
 <template>
+  <div>
+  <blockquote class="blockquote text-md-center">"If you can't explain it simply, you don't understand it." - Richard Feynman</blockquote>
+  <h3 class="text-md-center">HOW TO USE</h3>
+  <p class="text-md-center">
+    The layout is analogous to Office Hours. The TA visits each student with questions, and the interactions are live. <br>
+    Precious explanations can be saved as animations to benefit future students and teachers. 
+  </p>
+  <h3 class="text-md-center">For TAs</h3>
+  <p class="text-md-center">
+    Students with questions have red badges next to their names. Visit their rooms to see their questions. <br>
+    You can write directly on the whiteboard, type out an answer, or do both. 
+  </p>
+  <h3 class="text-md-center">For students</h3>
+  <p class="text-md-center">
+    When you submit a question in your "workspace", a red badge will appear next to your name to alert the TA. <br>
+    While the TA is helping, you may ask follow-up questions.
+  </p>
   <v-layout align-center justify-center row fill-height>
     <v-btn v-if="!user" @click="signIn()" color="pink darken--1 white--text">Sign in with Google</v-btn>
     <v-btn v-else @click="signOut()">Log out</v-btn>
   </v-layout>
+  </div>
 </template>
 
 <script>
