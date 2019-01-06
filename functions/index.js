641556c50b2e5cb72b6f107886c36cd2cd5b3b35
huB1erTi2 <hubewasi@gmail.com>
2019-01-06 15:12:36 +0100

    Added notifications progress from GitLab

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,82 @@
 const admin = require('firebase-admin')
 const functions = require('firebase-functions');
 const firebase_tools = require('firebase-tools')
-
-// https://github.com/firebase/snippets-node/tree/master/firestore/solution-deletes
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const webpush = require('web-push')
 
 admin.initializeApp()
 
-/**
+const firestore = admin.firestore()
+firestore.settings({timestampsInSnapshots: true})
+
+exports.notificationOnNewMessage = functions.firestore.document('/students/{uid}/messages/{mid]').onCreate((doc, context) => {
+	const params = context.params;
+
+	const messageId = params.mid;
+	const chatroomUid = params.uid;
+
+	const author = doc.author;
+	const authorName = author.displayName;
+	const authorUid = author.uid
+
+	receivers = firestore.collection('/students/' + chatroomUid + '/participants/')
+	receivers.listDocuments().then(documentRefs => {
+		return firestore.getAll(documentRefs)
+	}).then(receiverSnaps => {
+		documentSnaps.forEach(documentSnap => {
+			const currentUid = documentSnap.data().uid
+			const userDocRef = firestore.doc('/students/' + currentUid)
+			userDocRef.get().then(userDocSnap => {
+				if(userDocSnap.exists) {
+					var receiverSubscriptions = userDocSnap.data().subscriptions
+					if(!receiverSubscriptions) {
+						return;
+					}
+					receiverSubscriptions.forEach(function(sub) {
+						const subscription = JSON.parse(sub)
+						const payload = {
+							senderName,
+							body: message.content,
+							type: "message"
+						}
+						webpush.sendNotification(subscription, JSON.stringify(payload)).catch((err) => console.log(receiverUid + '\n' + sub + '\n' + err))
+					})
+				}
+				else {
+					console.log("Nonexistent user with uid given in participants\nUid: " + currentUid)
+					return null
+				}
+			})
+		})
+	})
+	receiverUids = receivers.map((receiver) => receiver.uid)
+	receiverUids.forEach(function(receiverUid) {
+		firestore.doc('/users/' + receiverUid).get().then(async snapshot => {
+			
+		})
+	})
+
+	return null;
+})
+
+/* AUTO-CREATED FUNCTIONS */
+
+exports.updateParticipantsForUserChatroom = functions.firestore.document('/students/{uid}/messages/{mid}').onCreate((doc, context) => {
+	const params = context.params;
+	const author = params.author;
+	const authorUid = author.uid;
+	const authorName = author.name
+
+	const colref = firebase.colection("/students/" + context.params.uid + "/participants");
+
+	colref.where("uid", "==", authorUid).then(querySnapshot => {
+		if(querySnapshot.empty) {
+			colref.add({
+				authorUid,
+				authorName
+			})
+		}
+	})
+})/**
  * Callable function that creates a custom auth token with the
  * custom attribute "admin" set to true.
  * 
@@ -80,6 +143,4 @@ exports.recursiveDelete = functions
         };
       });
   });
-
-
 
