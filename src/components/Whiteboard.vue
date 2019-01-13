77dd135cae61a902859958cd571761fadf4a2b05
Elton Lin <eltonlin1998@gmail.com>
2019-01-13 09:02:36 -0500

    simplify realtime whiteboard logic

--- src/components/Whiteboard.vue
+++ src/components/Whiteboard.vue
@@ -3,15 +3,15 @@
   <!-- https://zipso.net/a-simple-touchscreen-sketchpad-using-javascript-and-html5/ -->
   <div class="whiteboard">
     <template v-if="workspace">
-      <v-btn :loading="loading3"
-             :disabled="loading3"
+      <v-btn :loading="isReplaying"
+             :disabled="isReplaying"
              @click="initReplayLogic()">
         <span>Preview Replay</span>
         <span slot="loader">Replaying...</span>
       </v-btn>
       <template v-if="!workspace.isAnswered">
-        <v-btn :loading="loading2"
-               :disabled="loading2"
+        <v-btn :loading="isClearing"
+               :disabled="isClearing"
                @click="initClearBoardLogic()">
           <span>Clear Whiteboard</span>
           <span slot="loader">Clearing...</span>
@@ -48,23 +48,21 @@ export default {
   props: ['ownerUid', 'workspace'],
   data() {
     return {
-      loading2: false,
-      loading3: false,
       allStrokes: [],
       currentStroke: [],
       canvas: null,
       ctx: null,
+      isClearing: false,
+      isReplaying: false,
       touchX: null,
       touchY: null,
       lastX: -1,
       lastY: -1,
-      numOfStrokes: 0,
       unsubscribe: null,
       redrawTimeout: null 
     }
   },
   mounted() {
-    this.$root.$on('clear-whiteboard', this.deleteStrokesSubcollection) // listen to Navbar's "clear whiteboard" button
     this.$root.$on('save-explanation', docId => this.saveStrokes(docId))
     this.canvas = document.getElementById('myCanvas')
     this.ctx = this.canvas.getContext('2d')
@@ -82,12 +80,12 @@ export default {
       })
     },
     async initReplayLogic() {
-      this.loading3 = true
+      this.isReplaying = true
       await this.playAnimation()
-      this.loading3 = false 
+      this.isReplaying = false 
     },
     initClearBoardLogic() {
-      this.loading2 = true 
+      this.isClearing = true 
       this.deleteStrokesSubcollection()
       this.allStrokes = [] 
     },
@@ -110,17 +108,16 @@ export default {
         snapshot.docChanges().forEach(change => {
           if (change.type === 'added') {
             const stroke = change.doc.data()
-            this.allStrokes.push(stroke)
-            if (this.numOfStrokes == stroke.strokeNumber) {
+            if (this.allStrokes.length == stroke.strokeNumber) {
               return // board is already in sync 
             } else {
-              this.numOfStrokes += 1
               this.drawStroke(stroke.points)
+              this.allStrokes.push(stroke)
             }
           } 
           else if (change.type === 'removed') {
-            // for OTHER users to also clear canvas (since the current user's UI is already updated)
-            // currently inefficient
+            // to clear canvas for OTHER users (since the current user's UI is already updated)
+            // is inefficient
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
             this.resetVariables()
           }
@@ -142,12 +139,11 @@ export default {
     resetVariables() {
       this.allStrokes = []
       this.lastX = -1
-      this.numOfStrokes = 0
     },
     drawStroke(points) {
       for (let i = 0; i < points.length; i++) {
-        const x = points[i]['unitX'] * this.canvas.width
-        const y = points[i]['unitY'] * this.canvas.height
+        const x = points[i].unitX * this.canvas.width
+        const y = points[i].unitY * this.canvas.height
         this.drawLine(x, y, 3)
         if (i == points.length - 1) {
           this.lastX = -1 
@@ -159,37 +155,35 @@ export default {
       this.canvas.addEventListener('touchend',this.touchEnd, false)
       this.canvas.addEventListener('touchmove', this.touchMove, false)
     },
-    async deleteStrokesSubcollection() { // rename this function to deleteStrokesOnFirestore
+    async deleteStrokesSubcollection() {
       const path = `students/${this.ownerUid}/strokes`
       var deleteFn = firebase.functions().httpsCallable('recursiveDelete')
-     
       try {
         const result = await deleteFn({ path: path })
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
         this.resetVariables()
-        this.loading2 = false 
-        // this.$root.$emit('delete-whiteboard-strokes-success')
+        this.isClearing = false 
       } catch(err) {
         console.log('err =', err)
       }
     },
     drawLine(x, y, size=2) {
       if (this.lastX == -1) {
-        // means it's the start of the strokee
         this.lastX = x
         this.lastY = y
         return
       }
+      // set style 
       this.ctx.strokeStyle = 'purple'
       this.ctx.lineCap = 'round' // lines at different angles can join into each other
+      this.ctx.lineWidth = size
       // "trace" the line
       this.ctx.beginPath()
       this.ctx.moveTo(this.lastX, this.lastY)
       this.ctx.lineTo(x,y)
-      this.ctx.lineWidth = size
       // draw the line
       this.ctx.stroke()
-      // Update the last position to reference the current position
+      // update position
       this.lastX = x
       this.lastY = y
     },
@@ -211,61 +205,48 @@ export default {
       this.drawLine(this.touchX, this.touchY, 2)
     },
     touchEnd(e) {
-      const strokeNumber = this.numOfStrokes + 1 
-      this.numOfStrokes += 1
-      const strokesRef = db.collection('students').doc(this.ownerUid).collection('strokes')
-      strokesRef.doc(`${strokeNumber}`).set({
+      const strokeNumber = this.allStrokes.length + 1
+      const stroke = {
         points: this.currentStroke,
         author: this.author,
         strokeNumber
-      })
+      }
+      // save 
+      this.allStrokes.push(stroke)
+      const strokesRef = db.collection('students').doc(this.ownerUid).collection('strokes')
+      strokesRef.doc(`${strokeNumber}`).set(stroke)
+      // reset 
       this.currentStroke = []
       this.lastX = -1
     },
     getTouchPos(e) {
       if (e.touches) {
-        if (e.touches.length == 1) { // only deal with one finger
-          const touch = e.touches[0]; // get info for finger #1
-          // there should be an additional scrolling parameter to take into account 
-          // whiteboard size might become an issue
-          this.touchX = touch.pageX - this.canvas.getBoundingClientRect().left - window.scrollX
-          this.touchY = touch.pageY - this.canvas.getBoundingClientRect().top - window.scrollY
+        if (e.touches.length == 1) { 
+          const finger1 = e.touches[0] 
+          this.touchX = finger1.pageX - this.canvas.getBoundingClientRect().left - window.scrollX
+          this.touchY = finger1.pageY - this.canvas.getBoundingClientRect().top - window.scrollY
         }
       }
     },
-    getScreenHeight() {
-      return window.innerHeight
+    // ANIMATION RELATED FUNCTIONS
+    getDrawSpeed(n) {
+      if (n < 10) { return 500 } 
+      else if (n < 20) { return 250 } 
+      else if (n < 40) { return 100 } 
+      else { return 30 }
     },
-    // animation related 
     async playAnimation() {
-			if (!this.ctx || !this.canvas) {
+			if (!this.ctx || !this.canvas || !this.allStrokes) {
 				return
 			}
 			this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-			if (!this.allStrokes) {
-				return
-			}
-			if (this.allStrokes.length == 0) {
-				return
-			}
 			function timeout(ms) {
 				return new Promise(resolve => setTimeout(resolve, ms))
 			}
-			const strokes = this.allStrokes
-			const n = strokes.length
-			// determine drawing speed
-			let strokePeriod = 0
-			if (n < 10) {
-				strokePeriod = 500
-			} else if (n < 20) {
-				strokePeriod = 250
-			} else if (n < 40) {
-				strokePeriod = 100
-			} else {
-				strokePeriod = 30
-			}
-			for (let i = 0; i < n; i++) {
-				await this.drawPath(strokes[i], false) // draw incrementally, not instantly
+			const n = this.allStrokes.length
+			const strokePeriod = this.getDrawSpeed(n)
+			for (let i=0; i<n; i++) {
+				await this.drawPath(this.allStrokes[i], false) // draw incrementally, not instantly
 				await timeout(strokePeriod / 100)
 			}
 		},
@@ -274,17 +255,16 @@ export default {
 			if (data.isEraser) {
 				this.ctx.strokeStyle = 'white'
 				this.ctx.lineWidth = 20
-				this.ctx.lineCap = 'round'
 			} else {
 				this.ctx.strokeStyle = 'purple'
 				this.ctx.lineWidth = 2
-				this.ctx.lineCap = 'round'
 			}
+      this.ctx.lineCap = 'round '
       // draw 
 			const points = data.points
       for (let i = 0; i < points.length; i++) {
-        const x = points[i]['unitX'] * this.canvas.width
-        const y = points[i]['unitY'] * this.canvas.height
+        const x = points[i].unitX * this.canvas.width
+        const y = points[i].unitY * this.canvas.height
         this.drawLine(x, y, 3)
         if (!instant) {
           await timeout(this.pointPeriod)
