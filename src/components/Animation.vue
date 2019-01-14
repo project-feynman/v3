684967801a1321ec2139317b6b16824ad133c4db
Elton Lin <eltonlin1998@gmail.com>
2019-01-14 09:24:38 -0500

    refactor redraw() and drawLine() to the mixin

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -97,65 +97,6 @@ export default {
       this.$root.$emit('finish-loading-animation')
       this.drawAllStrokes(this.allStrokes)
     },
-    rescaleCanvas() {
-      this.canvas.width = this.canvas.scrollWidth
-      this.canvas.height = this.canvas.scrollHeight
-      // only redraw when the user has finished resizing the window
-      clearTimeout(this.redrawTimeout) // rescaleCanvas() called again during the 400 milliseconds, so cancel 
-      this.redrawTimeout = setTimeout(this.drawAllStrokes(this.allStrokes), 400) // resizing the canvas causes all drawings to be lost 
-    },
-    drawLine(x, y, size=3) {
-      if (this.lastX == -1) {
-        // means it's the start of the strokee
-        this.lastX = x
-        this.lastY = y
-        return
-      }
-      this.ctx.strokeStyle = 'purple'
-      this.ctx.lineCap = 'round' // lines at different angles can join into each other
-      // "trace" the line
-      this.ctx.beginPath()
-      this.ctx.moveTo(this.lastX, this.lastY)
-      this.ctx.lineTo(x,y)
-      this.ctx.lineWidth = size
-      // draw the line
-      this.ctx.stroke()
-      // Update the last position to reference the current position
-      this.lastX = x
-      this.lastY = y
-    },
-    async playAnimation() {
-			if (!this.ctx || !this.canvas) {
-				return
-			}
-			this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-			if (!this.allStrokes) {
-				return
-			}
-			if (this.allStrokes.length == 0) {
-				return
-			}
-			function timeout(ms) {
-				return new Promise(resolve => setTimeout(resolve, ms))
-			}
-			const strokes = this.allStrokes
-			const n = strokes.length
-			// determine drawing speed
-			let strokePeriod = 0
-			if (n < 10) {
-				strokePeriod = 500
-			} else if (n < 20) {
-				strokePeriod = 250
-			} else if (n < 40) {
-				strokePeriod = 100
-			} else {
-				strokePeriod = 30
-			}
-			for (let i = 0; i < n; i++) {
-				await this.drawPath(strokes[i], false) // draw incrementally, not instantly
-				await timeout(strokePeriod / 100)
-			}
-		}
   }
 }
 </script>
