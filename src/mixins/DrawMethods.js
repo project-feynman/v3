a66490f667480d93f80ece3235b5a992139c077b
Elton Lin <eltonlin1998@gmail.com>
2019-01-14 13:46:35 -0500

    ANNIHILATE SO MANY LINES OF UNNECESSARILY COMPLEX CODE

--- src/mixins/DrawMethods.js
+++ src/mixins/DrawMethods.js
@@ -1,57 +1,17 @@
 export default {
-  created() {
-    console.log('hello from mixin!')
-  },
   methods: {
     rescaleCanvas() {
+      // only redraw when the user has finished resizing the window
       this.canvas.width = this.canvas.scrollWidth
       this.canvas.height = this.canvas.scrollHeight
-      // only redraw when the user has finished resizing the window
       clearTimeout(this.redrawTimeout) // rescaleCanvas() called again during the 400 milliseconds, so cancel 
-      this.redrawTimeout = setTimeout(this.drawAllStrokes(this.allStrokes), 400) // resizing the canvas causes all drawings to be lost 
-    },
-    drawLine(x, y, size = 2) {
-      if (this.lastX == -1) {
-        this.lastX = x
-        this.lastY = y
-        return
-      }
-      // set style 
-      this.ctx.strokeStyle = 'purple'
-      this.ctx.lineCap = 'round' // lines at different angles can join into each other
-      this.ctx.lineWidth = size
-      // "trace" the line
-      this.ctx.beginPath()
-      this.ctx.moveTo(this.lastX, this.lastY)
-      this.ctx.lineTo(x,y)
-      // draw the line
-      this.ctx.stroke()
-      // update position
-      this.lastX = x
-      this.lastY = y
-    },
-    drawStroke(points) {
-      for (let i = 0; i < points.length; i++) {
-        const x = points[i].unitX * this.canvas.width
-        const y = points[i].unitY * this.canvas.height
-        this.drawLine(x, y, 3)
-        if (i == points.length - 1) {
-          this.lastX = -1 
-        }
-      }
-    },
-    drawAllStrokes(strokes) {
-      if (!strokes) {
-        return 
-      }
-      for (let i = 0; i < strokes.length; i++) {
-        this.drawStroke(strokes[i].points )
-      }
+      this.redrawTimeout = setTimeout(this.drawStrokesInstantly(), 400) // resizing the canvas causes all drawings to be lost 
     },
     async playVideo() {
-      // console.log('record button =', this.$refs)
-      this.isPlayingVideo = true 
+      // prevent the option of playing non-video supported video (because there was no recording in the first place)
+      // breaks if there are strokes rendering simultaneously 
       // this.$refs['record-button'].playRecording()
+      this.isPlayingVideo = true 
       this.currentTime = 0
       this.idx = 0 
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
@@ -60,9 +20,9 @@ export default {
         for (let i = startIdx; i < this.allStrokes.length; i++) {
           const nextStroke = this.allStrokes[i]
           if (Number(nextStroke.startTime) == this.currentTime.toFixed(1)) {
-            // specify the draw period 
             const strokePeriod = (nextStroke.endTime - nextStroke.startTime) * 1000
-            this.drawPath(nextStroke, false, strokePeriod) // draw incrementally, not instantly
+            const pointPeriod = strokePeriod / nextStroke.points.length 
+            this.drawStroke(nextStroke.points, pointPeriod)
             if (i == this.allStrokes.length - 1) {
               clearInterval(this.playProgress)
               this.isPlayingVisual = false 
@@ -77,60 +37,55 @@ export default {
       }
       this.playProgress = setInterval(checkWhetherStrokesShouldBePlayed, 100)
     },
-    async drawPath(data, instant = true, strokePeriod = 0) {
-      // initialize styles
-			if (data.isEraser) {
-				this.ctx.strokeStyle = 'white'
-				this.ctx.lineWidth = 20
-			} else {
-				this.ctx.strokeStyle = 'purple'
-				this.ctx.lineWidth = 2
-			}
-      this.ctx.lineCap = 'round '
-      // draw 
-			const points = data.points
-      const pointPeriod = strokePeriod / points.length
-      for (let i = 0; i < points.length; i++) {
-        const x = points[i].unitX * this.canvas.width
-        const y = points[i].unitY * this.canvas.height
-        this.drawLine(x, y, 3)
-        if (!instant) {
-          // handle when timestamps are all uniform 
-          // console.log('pointPeriod =', pointPeriod)
-          await timeout(pointPeriod)
-        }
-        if (i == points.length - 1) {
-          this.lastX = -1 
-        }
-      }
-			function timeout(ms) {
-				return new Promise(resolve => setTimeout(resolve, ms))
-			}
-			let promise = new Promise(resolve => setTimeout(resolve, 0))
-			promise.catch(error => console.log('error =', error))
-			return promise
-		},
-    // ANIMATION RELATED FUNCTIONS
-    getDrawSpeed(n) {
-      if (n < 10) { return 500 } 
-      else if (n < 20) { return 250 } 
-      else if (n < 40) { return 100 } 
-      else { return 30 }
-    },
-    async playAnimation() {
-			if (!this.ctx || !this.canvas || !this.allStrokes) {
-				return
-			}
+    async quickplay() {
 			this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-			function timeout(ms) {
-				return new Promise(resolve => setTimeout(resolve, ms))
-			}
-			const n = this.allStrokes.length
-			const strokePeriod = this.getDrawSpeed(n)
-			for (let i=0; i<n; i++) {
-				await this.drawPath(this.allStrokes[i], false) // draw incrementally, not instantly
-				await timeout(strokePeriod / 100)
-			}
-		}
+      for (const stroke of this.allStrokes) {
+        await this.drawStroke(stroke.points)
+      }
+		},
+    drawStrokesInstantly() {
+      for (const stroke of this.allStrokes) {
+        this.drawStroke(stroke.points, null)
+      }
+    },
+    // null is instant, 0 is quickplay, otherwise it's a realtime replay
+    async drawStroke(points, pointPeriod = 0) {
+      return new Promise(async resolve => {
+        this.setStyle()
+        for (const point of points) {
+          const x = point.unitX * this.canvas.width
+          const y = point.unitY * this.canvas.height
+          this.drawToPoint(x, y)
+          if (pointPeriod != null) {
+            await new Promise(resolve => setTimeout(resolve, pointPeriod)) // this line is really important, in that it's what makes the function take time to finish executing 
+          }
+        }
+        this.lastX = -1
+        resolve()
+      })  
+    },
+    drawToPoint(x, y) {
+      if (this.lastX == -1) {
+        this.lastX = x
+        this.lastY = y
+        return
+      }
+      this.setStyle()
+      this.traceLineTo(x, y)
+      this.ctx.stroke() 
+      // update position
+      this.lastX = x
+      this.lastY = y
+    },
+    setStyle() {
+      this.ctx.strokeStyle = 'purple'
+      this.ctx.lineCap = 'round' // lines at different angles can join into each other
+      this.ctx.lineWidth = 2
+    },
+    traceLineTo(x, y) {
+      this.ctx.beginPath()
+      this.ctx.moveTo(this.lastX, this.lastY)
+      this.ctx.lineTo(x,y)
+    }
   }
 }
