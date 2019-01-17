8296cb4a04852d00b1da29511728344f8de452d4
Elton Lin <eltonlin1998@gmail.com>
2019-01-17 11:17:30 -0500

    navbar updates correctly when switching between teacher pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div style="height: 100%;">
-
+        
+        <!-- RICHARD FEYNMAN'S QUOTE -->
         <transition name="fade">
           <template v-if="isFetchingUser">
             <v-layout align-center justify-center row fill-height wrap>
@@ -9,6 +10,7 @@
           </template>
         </transition>
 
+        <!-- LIST OF TEACHERS -->
         <template v-if="user">
           <div class="responsive-grid mt-5">
             <template v-for="teacher in teachers">
@@ -18,7 +20,6 @@
                     <v-card-title primary-title>
                       <div>
                         <div class="headline">{{ teacher.name }}</div>
-                        <!-- Below should be dynamic  -->
                         <span>{{ teacher.description }}</span>
                       </div>
                     </v-card-title>
@@ -32,67 +33,14 @@
           </div>
         </template>
 
+        <!-- SIGN-IN BUTTONS -->
         <template v-else>
           <v-layout align-center justify-center row fill-height wrap>
-             <v-btn @click="teacherSignIn()" color="error" dark large>SIGN UP AS A TEACHER</v-btn>
-             <v-btn @click="studentSignIn()" color="error" dark large>SIGN UP AS A STUDENT</v-btn>
+             <v-btn @click="teacherSignIn()" color="error" dark large>SIGN IN AS A TEACHER</v-btn>
+             <v-btn @click="studentSignIn()" color="error" dark large>SIGN IN AS A STUDENT</v-btn>
           </v-layout>
         </template>
 
-        <!-- <transition name="fade">
-          <template v-if="!isFetchingUser">
-                <p>
-          The most elegant explanations are often made by TAs while they are helping their students. However, year after year, these explanations are lost<br>
-          meaning other students don't benefit, and other TAs have to re-invent the wheel to explain recurring concepts.<br>
-
-          Feynman is a place where elegant explanations from TAs can be harnessed to evolve the world's academic content. 
-        </p>
-        <h3>How it works</h3>
-        <p>
-          Sal Khan wanted to help his nephew study, despite that she was in a different country. The videos he made formed the basis of KhanAcademy<br>
-          Here, each TA is a "Sal Khan", and each recitation student is a "nephew". The explanations TAs make will form the basis of Feynman.
-        </p>
-          </template>
-        </transition> -->
-        <!-- <transition name="fade">
-          <template v-if="!isFetchingUser">
-       <template v-if="user">
-        <h3>Introduction</h3>
-        <p>
-          The most elegant explanations are often made by TAs while they are helping their students. However, year after year, these explanations are lost<br>
-          meaning other students don't benefit, and other TAs have to re-invent the wheel to explain recurring concepts.<br>
-
-          Feynman is a place where elegant explanations from TAs can be harnessed to evolve the world's academic content. 
-        </p>
-        <h3>How it works</h3>
-        <p>
-          Sal Khan wanted to help his nephew study, despite that she was in a different country. The videos he made formed the basis of KhanAcademy<br>
-          Here, each TA is a "Sal Khan", and each recitation student is a "nephew". The explanations TAs make will form the basis of Feynman.
-        </p>
-        <h3>Teachers</h3>
-        <div class="responsive-grid">
-          <template v-for="TA in teachers">
-            <v-layout :key="TA.uid">
-              <v-flex>
-                <v-card>
-                  <v-card-title primary-title>
-                    <div>
-                      <h3 class="headline mb-0">{{ TA.name }}</h3>
-                      <div>You can write a sentence here</div>
-                    </div>
-                  </v-card-title>
-                  <v-card-actions>
-                    <v-btn flat color="orange" @click="$router.push(TA.uid)">Enter</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-flex>
-            </v-layout>
-          </template>
-        </div>
-        <v-btn @click="signOut()">Log out</v-btn>
-      </template>
-    </template>
-      </transition> -->
     </div>
 </template>
 
--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,5 +1,61 @@
 <template>
-  <div v-if="feedback">{{ feedback }}</div>
+  <div>
+    <div v-if="feedback">{{ feedback }}</div>
+          <!-- <transition name="fade">
+          <template v-if="!isFetchingUser">
+                <p>
+          The most elegant explanations are often made by TAs while they are helping their students. However, year after year, these explanations are lost<br>
+          meaning other students don't benefit, and other TAs have to re-invent the wheel to explain recurring concepts.<br>
+
+          Feynman is a place where elegant explanations from TAs can be harnessed to evolve the world's academic content. 
+        </p>
+        <h3>How it works</h3>
+        <p>
+          Sal Khan wanted to help his nephew study, despite that she was in a different country. The videos he made formed the basis of KhanAcademy<br>
+          Here, each TA is a "Sal Khan", and each recitation student is a "nephew". The explanations TAs make will form the basis of Feynman.
+        </p>
+          </template>
+        </transition> -->
+        <!-- <transition name="fade">
+          <template v-if="!isFetchingUser">
+       <template v-if="user">
+        <h3>Introduction</h3>
+        <p>
+          The most elegant explanations are often made by TAs while they are helping their students. However, year after year, these explanations are lost<br>
+          meaning other students don't benefit, and other TAs have to re-invent the wheel to explain recurring concepts.<br>
+
+          Feynman is a place where elegant explanations from TAs can be harnessed to evolve the world's academic content. 
+        </p>
+        <h3>How it works</h3>
+        <p>
+          Sal Khan wanted to help his nephew study, despite that she was in a different country. The videos he made formed the basis of KhanAcademy<br>
+          Here, each TA is a "Sal Khan", and each recitation student is a "nephew". The explanations TAs make will form the basis of Feynman.
+        </p>
+        <h3>Teachers</h3>
+        <div class="responsive-grid">
+          <template v-for="TA in teachers">
+            <v-layout :key="TA.uid">
+              <v-flex>
+                <v-card>
+                  <v-card-title primary-title>
+                    <div>
+                      <h3 class="headline mb-0">{{ TA.name }}</h3>
+                      <div>You can write a sentence here</div>
+                    </div>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-btn flat color="orange" @click="$router.push(TA.uid)">Enter</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </template>
+        </div>
+        <v-btn @click="signOut()">Log out</v-btn>
+      </template>
+    </template>
+      </transition> -->
+  </div>
 </template>
 
 <script>
@@ -25,9 +81,8 @@ export default {
     async initWorkspace() {
       if (!this.isFetchingUser && this.user) {
         const teacherUid = this.$route.params.teacher_id
-        const workspaceId = this.user.uid + '-' + teacherUid 
-        const ref = db.collection('workspaces').doc(workspaceId)
-          
+        const userWorkspaceId = this.user.uid + '-' + teacherUid 
+        const ref = db.collection('workspaces').doc(userWorkspaceId)
         const workspace = await ref.get()
         if (!workspace.exists) {
           let workspace = {
@@ -36,16 +91,17 @@ export default {
             teacherUid: this.$route.params.teacher_id,
             isOffice: false,
           }
-          console.log('this.user =', this.user)
           this.feedback = this.user 
-          if (this.user.isTeacher) {
+          if (this.user.isTeacher && this.user.uid == this.$route.params.teacher_id) {
             workspace.isOffice = true 
           }
           await ref.set(workspace)
           console.log('successfully created new workspace')
           this.feedback = 'successfully created a new workspace for you!'
+        } else {
+          console.log('redirecting to the user workspace')
         }
-        this.$router.push(`/${teacherUid}/workspace/${teacherUid}-${this.user.uid}`)
+        this.$router.push(`/${teacherUid}/workspace/${userWorkspaceId}`)
       }
     }
   }
