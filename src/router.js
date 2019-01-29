e2f9f140a2e0dbfd408c00df6a26f3a5ae5f0a2e
Elton Lin <eltonlin1998@gmail.com>
2019-01-29 14:46:20 -0500

    correct name of webpack chunk file

--- src/router.js
+++ src/router.js
@@ -21,7 +21,7 @@ export default new Router({
     },
     {
       path: '/:teacher_id/answer/:id',
-      component: () => import(/* webpackChunkName: "answers" */ './views/Answer.vue')
+      component: () => import(/* webpackChunkName: "answer" */ './views/Answer.vue')
     }
   ]
 })
