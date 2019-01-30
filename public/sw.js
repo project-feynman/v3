945f3835b94faf6dcf1a554b4d00aa425a8eb4cc
huBIerTi2 <hubi.wasilewski@gmail.com>
2019-01-30 16:33:46 +0100

    Changed sw.js so notifications are shown according to the new format of the web push message

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,8 @@
-self.addEventListener('install', function(event) {
-	console.log('worked')
-})
 self.addEventListener('push', function(event) {
   console.log(event.data.json())
 	const data = event.data.json()
-	if(data.type && data.type === 'message') {
-		registration.showNotification(data.senderName + " sent you a message...", {
+	if(data.type && data.type === 'notification') {
+		registration.showNotification(data.title, {
 			body: data.body
 		})
 	}
