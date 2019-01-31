9c3044f6ad114f09bcbf5f142468fa6514d1ad72
Elton Lin <eltonlin1998@gmail.com>
2019-01-31 15:43:27 -0500

    integrate initial setup of fullscreen popup

---
+++ src/components/BetaPopupButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout row justify-center>
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-btn slot="activator" color="primary" dark>Open Dialog</v-btn>
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-toolbar-title>Settings</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark flat @click="dialog = false">Save</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <slot>
+        
+        </slot>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        dialog: false,
+        notifications: false,
+        sound: true,
+        widgets: false
+      }
+    }
+  }
+</script>
