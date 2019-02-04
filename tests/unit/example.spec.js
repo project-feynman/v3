37a78179e1f463077ce21f1c9b3ec84c40ce2577
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2019-02-04 14:34:19 +0000

    added npm to travis cache

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,6 +1,3 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
-
 describe('FakeTest.vue', () => {
   it('passes no matter what', () => {
     expect(42).toBe(42)
