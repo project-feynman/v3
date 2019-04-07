0915cf2447ff29656b622803a22cae8043ccd3d2
Elton Lin <eltonlin1998@gmail.com>
2019-04-07 11:58:23 -0400

    MAN I FINALLY GOT VIDEO CHAT TO WORK!

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <video controls autoplay playsinline :srcObject.prop="todo.srcObject" :muted="todo.muted" :id="todo.id"></video>
+</template>
+
+<script>
+export default {
+  props: ['todo']
+}
+</script>
---
+++ src/views/VideoChat.vue
@@ -0,0 +1,163 @@
+<template>
+  <!-- https://github.com/muaz-khan/RTCMultiConnection/blob/master/demos/vuejs-video-conferencing.html -->
+  <div>
+    <h1>VIDEO CHAT</h1>
+    <input type="text" id="room-id" value="abcdef" autocorrect=off autocapitalize=off size=20>
+    <div id="open-room">Open Room</div> 
+    <div id="join-room">Join Room</div> 
+    <div id="open-or-join-room">Open or Join Room</div> 
+    <todo-item
+      v-for="item in videosList"
+      v-bind:todo="item"
+      v-bind:key="item.id">
+    </todo-item>
+    <div id="room-urls" style="text-align: center;display: none;background: #F1EDED;margin: 15px -10px;border: 1px solid rgb(189, 189, 189);border-left: 0;border-right: 0;"></div>
+  </div>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem.vue'
+
+export default {
+  components: {
+    TodoItem
+  },
+  data () {
+    return {
+      videosList: [],
+      connection: null
+    }
+  },
+  created () {
+    this.connection = new RTCMultiConnection()
+    // console.log('connection =', connection)
+    this.connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/'
+
+    this.connection.socketMessageEvent = 'video-conference-demo';
+    this.connection.session = {
+      audio: true,
+      video: true
+    }
+
+    this.connection.sdpConstraints.mandatory = {
+      OfferToReceiveAudio: true,
+      OfferToReceiveVideo: true
+    };
+
+    this.connection.autoCreateMediaElement = false;
+    this.connection.onstream = (event) => {
+        this.videosList.push({
+            id: event.streamid,
+            srcObject: event.stream,
+            muted: event.type === 'local'
+        });
+    };
+
+    this.connection.onstreamended = (event) => {
+        var newList = [];
+        this.videosList.forEach(function(item) {
+            if(item.id !== event.streamid) {
+                newList.push(item);
+            }
+        });
+        this.videosList = newList; 
+    };
+
+    console.log('connection =', this.connection)
+
+  },
+  mounted () {
+    document.getElementById('open-room').onclick = () => {
+      this.disableInputButtons();
+      this.connection.open(document.getElementById('room-id').value, () => {
+          this.showRoomURL(this.connection.sessionid);
+      });
+    };
+    document.getElementById('join-room').onclick = () => {
+      this.disableInputButtons();
+      this.connection.join(document.getElementById('room-id').value);
+    };
+    document.getElementById('open-or-join-room').onclick = () => {
+      this.disableInputButtons();
+      this.connection.openOrJoin(document.getElementById('room-id').value, (isRoomExist, roomid) => {
+          if (isRoomExist === false && this.connection.isInitiator === true) {
+            // if room doesn't exist, it means that current user will create the room
+            this.showRoomURL(roomid);
+          }
+      });
+    };
+
+    (function() {
+      var params = {},
+          r = /([^&=]+)=?([^&]*)/g;
+      function d(s) {
+          return decodeURIComponent(s.replace(/\+/g, ' '));
+      }
+      var match, search = window.location.search;
+      while (match = r.exec(search.substring(1)))
+          params[d(match[1])] = d(match[2]);
+      window.params = params;
+    })();
+
+    var roomid = '';
+    if (localStorage.getItem(this.connection.socketMessageEvent)) {
+        roomid = localStorage.getItem(this.connection.socketMessageEvent);
+    } else {
+        roomid = this.connection.token();
+    }
+    document.getElementById('room-id').value = roomid;
+    document.getElementById('room-id').onkeyup = () => {
+        localStorage.setItem(this.connection.socketMessageEvent, document.getElementById('room-id').value);
+    };
+    var hashString = location.hash.replace('#', '');
+    if (hashString.length && hashString.indexOf('comment-') == 0) {
+        hashString = '';
+    }
+    var roomid = params.roomid;
+    if (!roomid && hashString.length) {
+        roomid = hashString;
+    }
+    if (roomid && roomid.length) {
+        document.getElementById('room-id').value = roomid;
+        localStorage.setItem(this.connection.socketMessageEvent, roomid);
+        // auto-join-room
+        (function reCheckRoomPresence() {
+            this.connection.checkPresence(roomid, function(isRoomExist) {
+                if (isRoomExist) {
+                    this.connection.join(roomid);
+                    return;
+                }
+                setTimeout(reCheckRoomPresence, 5000);
+            });
+        })();
+        this.disableInputButtons();
+    }
+    // detect 2G
+    if(navigator.connection &&
+      navigator.connection.type === 'cellular' &&
+      navigator.connection.downlinkMax <= 0.115) {
+      alert('2G is not supported. Please use a better internet service.');
+    }
+  },
+  methods: {
+    disableInputButtons () {
+      document.getElementById('room-id').onkeyup();
+      document.getElementById('open-or-join-room').disabled = true;
+      document.getElementById('open-room').disabled = true;
+      document.getElementById('join-room').disabled = true;
+      document.getElementById('room-id').disabled = true;
+    },
+    showRoomURL (roomid) {
+      var roomHashURL = '#' + roomid;
+      var roomQueryStringURL = '?roomid=' + roomid;
+      var html = '<h2>Unique URL for your room:</h2><br>';
+      html += 'Hash URL: <a href="' + roomHashURL + '" target="_blank">' + roomHashURL + '</a>';
+      html += '<br>';
+      html += 'QueryString URL: <a href="' + roomQueryStringURL + '" target="_blank">' + roomQueryStringURL + '</a>';
+      var roomURLsDiv = document.getElementById('room-urls');
+      roomURLsDiv.innerHTML = html;
+      roomURLsDiv.style.display = 'block';
+    }
+  }
+}
+</script>
