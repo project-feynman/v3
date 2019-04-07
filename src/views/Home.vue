203e6adf0e696507adf43a7bc638ce3e35658e7d
Elton Lin <eltonlin1998@gmail.com>
2019-04-07 11:58:43 -0400

    Slow down initial rendering animation so it appears more natural

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -120,7 +120,7 @@ export default {
 }
 
 .fade-enter-active, .fade-leave-active {
-  transition: opacity .25s;
+  transition: opacity .5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
