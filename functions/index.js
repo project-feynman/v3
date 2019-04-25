09969c22b354d8d021e666e67829ae7f47de3370
Elton Lin <eltonlin1998@gmail.com>
2019-04-25 17:46:59 -0400

    Sync user's online status across Vuex, Firebase and Firestore

--- functions/index.js
+++ functions/index.js
@@ -1,269 +1,305 @@
-const admin = require('firebase-admin')
+// const admin = require('firebase-admin')
+// const functions = require('firebase-functions')
+// const firebase_tools = require('firebase-tools')
+// const webpush = require('web-push')
+// const nodemailer = require('nodemailer')
+// const config = require('./config')
+// const adminCredentials = require('./feynman-mvp-firebase-adminsdk-3zyg9-7ce076beb3.json')
+
+// admin.initializeApp({
+// 	credential: admin.credential.cert(adminCredentials),
+// 	databaseUrl: "https://feynman-mvp.firebaseio.com"
+// })
+
+// const vapidKeys = config.vapidKeys
+// webpush.setVapidDetails(
+// 	'mailto:hubewasi@gmail.com',
+// 	vapidKeys.publicKey,
+// 	vapidKeys.privateKey
+// )
+
+// const gmailPass = config.gmailPass
+// const oauth = config.oauth
+// const transporter = nodemailer.createTransport({
+// 	host: 'smtp.gmail.com',
+// 	port: 465,
+// 	secure: true,
+// 	service: 'Gmail',
+// 	auth: oauth
+// })
+
+// const firestore = admin.firestore()
+// firestore.settings({timestampsInSnapshots: true})
+
 const functions = require('firebase-functions');
-const firebase_tools = require('firebase-tools')
-const webpush = require('web-push')
-const nodemailer = require('nodemailer')
-const config = require('./config')
-const adminCredentials = require('./feynman-mvp-firebase-adminsdk-3zyg9-421740ce6a.json')
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-admin.initializeApp({
-	credential: admin.credential.cert(adminCredentials),
-	databaseUrl: "https://feynman-mvp.firebaseio.com"
-})
+// Since this code will be running in the Cloud Functions environment
+// we call initialize Firestore without any arguments because it
+// detects authentication from the environment.
+const firestore = admin.firestore();
 
-const vapidKeys = config.vapidKeys
-webpush.setVapidDetails(
-	'mailto:hubewasi@gmail.com',
-	vapidKeys.publicKey,
-	vapidKeys.privateKey
+exports.onUserStatusChanged = functions.database.ref('/status/{uid}').onUpdate(
+  async (change, context) => {
+    // Get the data written to Realtime Database
+    const eventStatus = change.after.val()
+
+    // Then use other event data to create a reference to the
+    // corresponding Firestore document.
+    const userStatusFirestoreRef = firestore.doc(`/users/${context.params.uid}`)
+
+    // multiple requests may have been triggered, but earlier requests might not actually resolve earlier 
+    // therefore it's necessary to compare the timestamps
+    const statusSnapshot = await change.after.ref.once('value')
+    const status = statusSnapshot.val()
+    console.log(status, eventStatus)
+    // If the current timestamp for this data is newer than
+    // the data that triggered this event, we exit this function.
+    if (status.last_changed > eventStatus.last_changed) {
+      return null
+    } else {
+      // eventStatus.last_changed = new Date(eventStatus.last_changed)
+      return userStatusFirestoreRef.update(eventStatus)
+    }
+  }
 )
 
-const gmailPass = config.gmailPass
-const oauth = config.oauth
-const transporter = nodemailer.createTransport({
-	host: 'smtp.gmail.com',
-	port: 465,
-	secure: true,
-	service: 'Gmail',
-	auth: oauth
-})
-const firestore = admin.firestore()
-firestore.settings({timestampsInSnapshots: true})
 
-exports.notificationOnNewMessage = functions.firestore.document('/workspaces/{wid}/messages/{mid}').onCreate((doc, context) => {
-	_updateParticipants()
+// exports.notificationOnNewMessage = functions.firestore.document('/workspaces/{wid}/messages/{mid}').onCreate((doc, context) => {
+// 	_updateParticipants()
 	
-	const params = context.params;
-	const workspaceDocSnap = doc
-	const workspaceDocData = workspaceDocumentSnap.data()
+// 	const params = context.params;
+// 	const workspaceDocSnap = doc
+// 	const workspaceDocData = workspaceDocumentSnap.data()
 
-	const messageId = params.mid;
-	const chatroomId = params.wid;
+// 	const messageId = params.mid;
+// 	const chatroomId = params.wid;
 
-	const author = workspaceDocData.author;
-	const authorName = author.name;
-	const authorUid = author.uid
-	const messageContent = workspaceDocData.content
-	const messageTimestamp = workspaceDocData.timestamp
+// 	const author = workspaceDocData.author;
+// 	const authorName = author.name;
+// 	const authorUid = author.uid
+// 	const messageContent = workspaceDocData.content
+// 	const messageTimestamp = workspaceDocData.timestamp
 
-	participantsDoc = firestore.doc('/workspaces/' + chatroomId + '/participants/')
+// 	participantsDoc = firestore.doc('/workspaces/' + chatroomId + '/participants/')
 
-	participantsDoc.get().then(participantsDocSnap => {
-		const participantsDocData = participantsDocSnap.data()
-		const participantsUids = participantsDocData.participants
+// 	participantsDoc.get().then(participantsDocSnap => {
+// 		const participantsDocData = participantsDocSnap.data()
+// 		const participantsUids = participantsDocData.participants
 
-		participantsUids.forEach(participantUid => {
-			if(participantUid !== authorUid) {
-				_sendNotificationByUid(participantUid, authorName + " sent a message...", messageContent)	
-			}
-		})
-	})
+// 		participantsUids.forEach(participantUid => {
+// 			if(participantUid !== authorUid) {
+// 				_sendNotificationByUid(participantUid, authorName + " sent a message...", messageContent)	
+// 			}
+// 		})
+// 	})
 
-	return null;
-})
+// 	return null;
+// })
 
-exports.emailOnStudentHelp = functions.firestore.document('/workspaces/{wid}').onUpdate((change, context) => {
-	const workspaceDocDataBefore = change.before.data()
-	const workspaceDocDataAfter = change.after.data()
-	const params = context.params
+// exports.emailOnStudentHelp = functions.firestore.document('/workspaces/{wid}').onUpdate((change, context) => {
+// 	const workspaceDocDataBefore = change.before.data()
+// 	const workspaceDocDataAfter = change.after.data()
+// 	const params = context.params
 
-	const askingBefore = workspaceDocDataBefore.isAskingQuestion
-	const askingAfter = workspaceDocDataAfter.isAskingQuestion
-	// if before they werent asking and after they are, then it's the change i'm interested in
-	// otherwise end the execution
-	if(!(workspaceDocDataAfter.isAskingQuestion === true && workspaceDocDataBefore.isAskingQuestion === false)) {
-		return
-	}
+// 	const askingBefore = workspaceDocDataBefore.isAskingQuestion
+// 	const askingAfter = workspaceDocDataAfter.isAskingQuestion
+// 	// if before they werent asking and after they are, then it's the change i'm interested in
+// 	// otherwise end the execution
+// 	if(!(workspaceDocDataAfter.isAskingQuestion === true && workspaceDocDataBefore.isAskingQuestion === false)) {
+// 		return
+// 	}
 
-	const askerUid = workspaceDocDataBefore.ownerUid
-	const askerName = workspaceDocDataBefore.ownerName
-	const askerFirstName = askerName.split(' ')[0]
+// 	const askerUid = workspaceDocDataBefore.ownerUid
+// 	const askerName = workspaceDocDataBefore.ownerName
+// 	const askerFirstName = askerName.split(' ')[0]
     
-	const workspaceOwnerUid = workspaceDocDataBefore.teacherUid
-	const workspaceId = params.wid
+// 	const workspaceOwnerUid = workspaceDocDataBefore.teacherUid
+// 	const workspaceId = params.wid
 
-	const workspaceUrl = "https://feynman.online/" + workspaceOwnerUid + "/workspace/" + workspaceId
+// 	const workspaceUrl = "https://feynman.online/" + workspaceOwnerUid + "/workspace/" + workspaceId
 
 
-	firestore.collection('/workspaces/').where('teacherUid', '==', workspaceOwnerUid).where('isOffice', '==', true).get().then(assistantsQuerySnap => {
-		const assistantsQueryDocsSnaps = assistantsQuerySnap.docs
-		assistantsQueryDocsSnaps.forEach(assitantQueryDocSnap => {
-			const assistantQueryDocData = assitantQueryDocSnap.data()
-			_sendEmailByUid(
-				assistantQueryDocData.ownerUid,
-				askerFirstName + " asked a question on Feynman.",
-				askerName + " asked a question in a workspace you're a TA in. Here's the link to the workspace: " + workspaceUrl,
-				"<p>" + askerName + " asked a question in a workspace you're a TA in. Click <a href=\"" + workspaceUrl + "\">here</a> to go there. "
-			)
-		})
-	})
+// 	firestore.collection('/workspaces/').where('teacherUid', '==', workspaceOwnerUid).where('isOffice', '==', true).get().then(assistantsQuerySnap => {
+// 		const assistantsQueryDocsSnaps = assistantsQuerySnap.docs
+// 		assistantsQueryDocsSnaps.forEach(assitantQueryDocSnap => {
+// 			const assistantQueryDocData = assitantQueryDocSnap.data()
+// 			_sendEmailByUid(
+// 				assistantQueryDocData.ownerUid,
+// 				askerFirstName + " asked a question on Feynman.",
+// 				askerName + " asked a question in a workspace you're a TA in. Here's the link to the workspace: " + workspaceUrl,
+// 				"<p>" + askerName + " asked a question in a workspace you're a TA in. Click <a href=\"" + workspaceUrl + "\">here</a> to go there. "
+// 			)
+// 		})
+// 	})
 	
-	// SUBJECT: "{first NAME} asked a question
-	// BODY: "{NAME} asked a question in a workspace you're a TA in. Here's the link to the workspace: {LINK}"
-	// HTML: same but LINK is an href
-})
+// 	// SUBJECT: "{first NAME} asked a question
+// 	// BODY: "{NAME} asked a question in a workspace you're a TA in. Here's the link to the workspace: {LINK}"
+// 	// HTML: same but LINK is an href
+// })
 
 
-exports.updateParticipants = functions.https.onCall((data, context) => {
-	_updateParticipants(data.workspaceid)
-})
+// exports.updateParticipants = functions.https.onCall((data, context) => {
+// 	_updateParticipants(data.workspaceid)
+// })
 
-function _updateParticipants(workspaceId) {
-	//WIP
-	const participantsDoc = firestore.doc('/workspaces/' + workspaceId + '/participants/')
+// function _updateParticipants(workspaceId) {
+// 	//WIP
+// 	const participantsDoc = firestore.doc('/workspaces/' + workspaceId + '/participants/')
 
-	participantsDoc.get().then(participantsDocSnap => {
-		const participantsDocData = participantsDocSnap.data()
-		participantsDocData.participants.forEach(participant => {
-		})
-	})
-}
+// 	participantsDoc.get().then(participantsDocSnap => {
+// 		const participantsDocData = participantsDocSnap.data()
+// 		participantsDocData.participants.forEach(participant => {
+// 		})
+// 	})
+// }
 
-exports.sendEmailByUid = functions.https.onCall((data, context) => {
-	_sendEmailByUid(data.uid, data.subject, data.body, data.html)
-})
+// exports.sendEmailByUid = functions.https.onCall((data, context) => {
+// 	_sendEmailByUid(data.uid, data.subject, data.body, data.html)
+// })
 
-function _sendEmailByUid(uid, subject, body, html) {
-	admin.auth().getUser(uid).then(user => {
-		const userEmail = user.email
-		_sendEmail(userEmail, subject, body, html)
-	})
-}
+// function _sendEmailByUid(uid, subject, body, html) {
+// 	admin.auth().getUser(uid).then(user => {
+// 		const userEmail = user.email
+// 		_sendEmail(userEmail, subject, body, html)
+// 	})
+// }
 
-function _sendEmail(email, subject, text, html) {
+// function _sendEmail(email, subject, text, html) {
 
-	const message = {
-		from: '"Feynman Notifications" feynmannotif@gmail.com',
-		to: email,
-		subject,
-		text,
-		html
-	}
+// 	const message = {
+// 		from: '"Feynman Notifications" feynmannotif@gmail.com',
+// 		to: email,
+// 		subject,
+// 		text,
+// 		html
+// 	}
 
-	transporter.sendMail(message, (error, response) => {
-		console.log(error)
-		console.log(response)
-	})
-}
-exports.sendNotificationByUid = functions.https.onCall((data, context) => {
-	_sendNotificationByUid(data.uid, data.title, data.body)
-})
+// 	transporter.sendMail(message, (error, response) => {
+// 		console.log(error)
+// 		console.log(response)
+// 	})
+// }
+// exports.sendNotificationByUid = functions.https.onCall((data, context) => {
+// 	_sendNotificationByUid(data.uid, data.title, data.body)
+// })
 
-function _sendNotificationByUid(uid, title, body) {
-	const userDocRef = firestore.doc('/users/' + uid)
-	userDocRef.get().then(userDocSnap => {
-		if(userDocSnap.exists) {
-			firestore.collection('/users/' + uid + '/subscriptions/').get().then(subscriptionDocs => {
-				if(subscriptionDocs.docs == 0) {
-					return;
-				}
-				subscriptionDocs.forEach(function(subscriptionDoc) {
-					const subscriptionDocData = subscriptionDoc.data()
-					console.log(subscriptionDocData)
-					const subscription = subscriptionDocData.subscription
-					const JSONsubscription = JSON.parse(subscription)
-					const payload = {
-						title,
-						body,
-						type: "notification"
-					}
-					webpush.sendNotification(JSONsubscription, JSON.stringify(payload))
-					.catch((err) => {
-						console.log("error while sending notification to uid: " + uid + " subscription: " + subscription + "\n" + err)
-					})
-				})
-			})
-		}
-		else {
-			console.log("Uid not linked to any user or undefined\nUID: " + uid)
-			return null
-		}
-	})
-}
+// function _sendNotificationByUid(uid, title, body) {
+// 	const userDocRef = firestore.doc('/users/' + uid)
+// 	userDocRef.get().then(userDocSnap => {
+// 		if(userDocSnap.exists) {
+// 			firestore.collection('/users/' + uid + '/subscriptions/').get().then(subscriptionDocs => {
+// 				if(subscriptionDocs.docs == 0) {
+// 					return;
+// 				}
+// 				subscriptionDocs.forEach(function(subscriptionDoc) {
+// 					const subscriptionDocData = subscriptionDoc.data()
+// 					console.log(subscriptionDocData)
+// 					const subscription = subscriptionDocData.subscription
+// 					const JSONsubscription = JSON.parse(subscription)
+// 					const payload = {
+// 						title,
+// 						body,
+// 						type: "notification"
+// 					}
+// 					webpush.sendNotification(JSONsubscription, JSON.stringify(payload))
+// 					.catch((err) => {
+// 						console.log("error while sending notification to uid: " + uid + " subscription: " + subscription + "\n" + err)
+// 					})
+// 				})
+// 			})
+// 		}
+// 		else {
+// 			console.log("Uid not linked to any user or undefined\nUID: " + uid)
+// 			return null
+// 		}
+// 	})
+// }
 
-exports.updateParticipantsForUserChatroom = functions.firestore.document('/users/{uid}/messages/{mid}').onCreate((doc, context) => {
-	const params = context.params;
-	const author = params.author;
-	const authorUid = author.uid;
-	const authorName = author.name
+// exports.updateParticipantsForUserChatroom = functions.firestore.document('/users/{uid}/messages/{mid}').onCreate((doc, context) => {
+// 	const params = context.params;
+// 	const author = params.author;
+// 	const authorUid = author.uid;
+// 	const authorName = author.name
 
-	const colref = firebase.colection("/users/" + context.params.uid + "/participants");
+// 	const colref = firebase.colection("/users/" + context.params.uid + "/participants");
 
-	colref.where("uid", "==", authorUid).then(querySnap => {
-		if(querySnap.empty) {
-			colref.add({
-				authorUid,
-				authorName
-			})
-		}
-	})
-})
-/* AUTO-CREATED FUNCTIONS */
-/**
- * Callable function that creates a custom auth token with the
- * custom attribute "admin" set to true.
- * 
- * See https://firebase.google.com/docs/auth/admin/create-custom-tokens
- * for more information on creating custom tokens.
- * 
- * @param {string} data.uid the user UID to set on the token.
- */
-exports.mintAdminToken = functions.https.onCall((data, context) => {
-  const uid = data.uid;
+// 	colref.where("uid", "==", authorUid).then(querySnap => {
+// 		if(querySnap.empty) {
+// 			colref.add({
+// 				authorUid,
+// 				authorName
+// 			})
+// 		}
+// 	})
+// })
+// /* AUTO-CREATED FUNCTIONS */
+// /**
+//  * Callable function that creates a custom auth token with the
+//  * custom attribute "admin" set to true.
+//  * 
+//  * See https://firebase.google.com/docs/auth/admin/create-custom-tokens
+//  * for more information on creating custom tokens.
+//  * 
+//  * @param {string} data.uid the user UID to set on the token.
+//  */
+// exports.mintAdminToken = functions.https.onCall((data, context) => {
+//   const uid = data.uid;
 
-  return admin
-    .auth()
-    .createCustomToken(uid, { admin: true })
-    .then(function(token) {
-      return { token: token };
-    });
-});
+//   return admin
+//     .auth()
+//     .createCustomToken(uid, { admin: true })
+//     .then(function(token) {
+//       return { token: token };
+//     });
+// });
 
-// [START recursive_delete_function]
-/**
- * Initiate a recursive delete of documents at a given path.
- * 
- * The calling user must be authenticated and have the custom "admin" attribute
- * set to true on the auth token.
- * 
- * This delete is NOT an atomic operation and it's possible
- * that it may fail after only deleting some documents.
- * 
- * @param {string} data.path the document or collection path to delete.
- */
-exports.recursiveDelete = functions
-  .runWith({
-    timeoutSeconds: 540,
-    memory: '2GB'
-  })
-  .https.onCall((data, context) => {
-    // // Only allow admin users to execute this function.
-    // if (!(context.auth && context.auth.token && context.auth.token.admin)) {
-    //   throw new functions.https.HttpsError(
-    //     'permission-denied',
-    //     'Must be an administrative user to initiate delete.'
-    //   );
-    // }
+// // [START recursive_delete_function]
+// /**
+//  * Initiate a recursive delete of documents at a given path.
+//  * 
+//  * The calling user must be authenticated and have the custom "admin" attribute
+//  * set to true on the auth token.
+//  * 
+//  * This delete is NOT an atomic operation and it's possible
+//  * that it may fail after only deleting some documents.
+//  * 
+//  * @param {string} data.path the document or collection path to delete.
+//  */
+// exports.recursiveDelete = functions
+//   .runWith({
+//     timeoutSeconds: 540,
+//     memory: '2GB'
+//   })
+//   .https.onCall((data, context) => {
+//     // // Only allow admin users to execute this function.
+//     // if (!(context.auth && context.auth.token && context.auth.token.admin)) {
+//     //   throw new functions.https.HttpsError(
+//     //     'permission-denied',
+//     //     'Must be an administrative user to initiate delete.'
+//     //   );
+//     // }
 
-    const path = data.path;
-    console.log(
-      `User ${context.auth.uid} has requested to delete path ${path}`
-    );
+//     const path = data.path;
+//     console.log(
+//       `User ${context.auth.uid} has requested to delete path ${path}`
+//     );
 
-    // Run a recursive delete on the given document or collection path.
-    // The 'token' must be set in the functions config, and can be generated
-    // at the command line by running 'firebase login:ci'.
-    return firebase_tools.firestore
-      .delete(path, {
-        project: process.env.GCLOUD_PROJECT,
-        recursive: true,
-        yes: true,
-        token: functions.config().fb.token
-      })
-      .then(() => {
-        return {
-          path: path 
-        };
-      });
-  });
+//     // Run a recursive delete on the given document or collection path.
+//     // The 'token' must be set in the functions config, and can be generated
+//     // at the command line by running 'firebase login:ci'.
+//     return firebase_tools.firestore
+//       .delete(path, {
+//         project: process.env.GCLOUD_PROJECT,
+//         recursive: true,
+//         yes: true,
+//         token: functions.config().fb.token
+//       })
+//       .then(() => {
+//         return {
+//           path: path 
+//         };
+//       });
+//   });
 
--- src/store.js
+++ src/store.js
@@ -1,48 +1,84 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import db from '@/database.js'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/database'
 
 Vue.use(Vuex)
+
+function setDisconnectHook(user) {
+	const firebaseRef = firebase.database().ref('/status/' + user.uid)
+	let isOffline = {
+		isOnline: false,
+		last_changed: firebase.firestore.FieldValue.serverTimestamp()
+	}
+	let isOnline = {
+		isOnline: true,
+		last_changed: firebase.firestore.FieldValue.serverTimestamp()
+	}
+
+	firebase.database().ref('.info/connected').on('value', async snapshot => {
+    console.log('snapshot.val() =', snapshot.val())
+    if (snapshot.val() == false) { return } 
+    console.log('setting the onDisconnect() hook...')
+    await firebaseRef.onDisconnect().set(isOffline) // server now knows if connection is lost, perform "set(isOfflineForDatabase)"
+    // this is a lie
+    firebaseRef.set(isOnline)
+    // redundant, but MUCH faster
+    firebase.firestore().collection('users').doc(user.uid).update({
+      isOnline: true
+    })
+  })
+}
+
+function syncFirestoreToVuex(ref) {
+  ref.onSnapshot(user => {
+    context.commit('SET_USER', user.data())
+    setDisconnectHook(user)
+  }) 
+}
+
+// ref.onSnapshot(mirror => {
+//   context.commit('setUser', mirror.data())
+//   if (!context.state.handledOnlineStatus) {
+//     checkOnlineStatusAndSetDisconnectHook(mirror.data())
+//     context.commit('setHandledOnlineStatus', true)
+//   }
+// })
 
 export default new Vuex.Store({
   state: {
     user: null,
-    isFetchingUser: true,
-    creatingTeacher: false
+    isFetchingUser: true
   },
   mutations: {
     SET_USER(state, user) {
       state.user = user 
-      // quick-fix
       state.isFetchingUser = false 
-    },
-    SET_CREATING_TEACHER(state, boolean) {
-      state.creatingTeacher = boolean 
     }
   },
   actions: {
-    async handleUserLogic(context, { nickname, uid, email }) {
-      // console.log('handleUserLogic()')
-      // console.log('nickname =', nickname)
-      let simplifiedUser = {
-        uid,
-        email,
-        nickname: "Anonymous"
-      }
-      if (nickname) {
-        simplifiedUser.nickname = nickname 
-      }
+    async handleUserLogic(context, { uid, email }) {
+      let simplifiedUser = { uid, email }
       context.commit('SET_USER', simplifiedUser) // commit the user to avoid blocking page load 
+      // update its "isOnline" statement later
       const userRef = db.collection('users').doc(uid) 
       const mirrorUser = await userRef.get() 
       if (mirrorUser.exists) {
-        context.commit('SET_USER', mirrorUser.data())
+        // now set up syncing with the user copy
+          userRef.onSnapshot(user => {
+            context.commit('SET_USER', user.data())
+            setDisconnectHook({ uid })
+          }) 
       } else {
+        // create a new account
         userRef.set(simplifiedUser)
-        console.log('simplifiedUser =', simplifiedUser)
-        context.commit('SET_USER', simplifiedUser)
-        console.log('successfully created mirror user')
-        // console.log('user document fetched')
+        // now set up syncing with the user copy
+        userRef.onSnapshot(user => {
+          context.commit('SET_USER', user.data())
+          setDisconnectHook({ uid })
+        }) 
       }
     }
   }
