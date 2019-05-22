dbd9498480717ea5c93887511077c053b61350bf
Elton Lin <eltonlin1998@gmail.com>
2019-05-22 18:09:54 -0400

    Fix RequestAnimation bug and somehow drawing is smoother now

--- src/components/Whiteboard.vue
+++ src/components/Whiteboard.vue
@@ -178,11 +178,7 @@ export default {
       this.setStyle(this.color, this.lineWidth)
       this.getTouchPos(e) 
       this.convertAndSavePoint(this.touchX, this.touchY)
-      function callback(x, y) {
-        return () => this.drawToPoint(x, y)
-      }
-      window.requestAnimationFrame(callback(this.touchX, this.touchY))
-      // this.drawToPoint(this.touchX, this.touchY)
+      this.drawToPoint(this.touchX, this.touchY)
       if (this.currentTime) {
         this.startTime = this.currentTime.toFixed(1)
       }
@@ -197,11 +193,7 @@ export default {
       e.preventDefault()
       this.getTouchPos(e)
       this.convertAndSavePoint(this.touchX, this.touchY)
-      function callback(x, y) {
-        return () => this.drawToPoint(x, y)
-      }
-      window.requestAnimationFrame(callback(this.touchX, this.touchY))
-      // this.drawToPoint(this.touchX, this.touchY)
+      this.drawToPoint(this.touchX, this.touchY)
     },
     touchEnd(e) {
       if (this.currentStroke.length == 0) {
