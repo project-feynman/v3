95a1bff595f44400e1e4172cb0dfe2e1b025e0d8
Elton Lin <eltonlin1998@gmail.com>
2019-05-23 00:33:51 -0400

    Update the recursive delete function to work properly

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 // const admin = require('firebase-admin')
 // const functions = require('firebase-functions')
-// const firebase_tools = require('firebase-tools')
+
 // const webpush = require('web-push')
 // const nodemailer = require('nodemailer')
 // const config = require('./config')
@@ -27,7 +27,7 @@
 // 	service: 'Gmail',
 // 	auth: oauth
 // })
-
+const firebase_tools = require('firebase-tools')
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp()
@@ -296,39 +296,39 @@ exports.onWorkspaceParticipantsChanged = functions.database.ref('/workspace/{cla
 //  * 
 //  * @param {string} data.path the document or collection path to delete.
 //  */
-// exports.recursiveDelete = functions
-//   .runWith({
-//     timeoutSeconds: 540,
-//     memory: '2GB'
-//   })
-//   .https.onCall((data, context) => {
-//     // // Only allow admin users to execute this function.
-//     // if (!(context.auth && context.auth.token && context.auth.token.admin)) {
-//     //   throw new functions.https.HttpsError(
-//     //     'permission-denied',
-//     //     'Must be an administrative user to initiate delete.'
-//     //   );
-//     // }
+exports.recursiveDelete = functions
+  .runWith({
+    timeoutSeconds: 540,
+    memory: '2GB'
+  })
+  .https.onCall((data, context) => {
+    // // Only allow admin users to execute this function.
+    // if (!(context.auth && context.auth.token && context.auth.token.admin)) {
+    //   throw new functions.https.HttpsError(
+    //     'permission-denied',
+    //     'Must be an administrative user to initiate delete.'
+    //   );
+    // }
 
-//     const path = data.path;
-//     console.log(
-//       `User ${context.auth.uid} has requested to delete path ${path}`
-//     );
+    const path = data.path;
+    console.log(
+      `User ${context.auth.uid} has requested to delete path ${path}`
+    )
 
-//     // Run a recursive delete on the given document or collection path.
-//     // The 'token' must be set in the functions config, and can be generated
-//     // at the command line by running 'firebase login:ci'.
-//     return firebase_tools.firestore
-//       .delete(path, {
-//         project: process.env.GCLOUD_PROJECT,
-//         recursive: true,
-//         yes: true,
-//         token: functions.config().fb.token
-//       })
-//       .then(() => {
-//         return {
-//           path: path 
-//         };
-//       });
-//   });
+    // Run a recursive delete on the given document or collection path.
+    // The 'token' must be set in the functions config, and can be generated
+    // at the command line by running 'firebase login:ci'.
+    return firebase_tools.firestore
+      .delete(path, {
+        project: process.env.GCLOUD_PROJECT,
+        recursive: true,
+        yes: true,
+        token: functions.config().fb.token
+      })
+      .then(() => {
+        return {
+          path: path 
+        }
+      })
+  });
 
