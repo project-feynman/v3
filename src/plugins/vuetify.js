733fa865cdb1d9acac6ab3b5bebd851db1779516
Lujing Cen <lujing@mit.edu>
2019-09-30 18:16:45 -0400

    upgrade vuetify and fix error messages

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,24 +1,10 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: 'md',
+    iconfont: 'mdi',
   },
-  theme: {
-    themes: {
-      light: {
-        primary: colors.blue, // #E53935
-        secondary: colors.purple, // #FFCDD2
-        accent: colors.blue, // #3F51B5
-      },
-    },
-  }
 });
-
-//  primary: colors.blue, // #E53935
-//     secondary: colors.purple, // #FFCDD2
-//     accent: colors.blue // #3F51B5
