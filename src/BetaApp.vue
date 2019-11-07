6a72f0ab5a221b3e657c80f7f1734a040c2a2326
Elton Lin <eltonlin1998@gmail.com>
2019-11-07 02:09:42 -0500

    Remove obsolete data

--- src/BetaApp.vue
+++ src/BetaApp.vue
@@ -19,26 +19,12 @@ export default {
     TheSideDrawer
   },
   data: () => ({
-    drawer: true,
-    items: [
-      { icon: 'trending_up', text: 'Most Popular' },
-      { icon: 'subscriptions', text: 'Subscriptions' },
-      { icon: 'history', text: 'History' },
-      { icon: 'featured_play_list', text: 'Playlists' },
-      { icon: 'watch_later', text: 'Watch Later' },
-    ],
-    items2: [
-      { picture: 28, text: 'Joseph' },
-      { picture: 38, text: 'Apple' },
-      { picture: 48, text: 'Xbox Ahoy' },
-      { picture: 58, text: 'Nokia' },
-      { picture: 78, text: 'MKBHD' },
-    ],
+    drawer: true
   }),
   created () {
     this.$root.$on("toggle-drawer", () => {
       this.drawer = !this.drawer
     })
-  },
+  }
 }
 </script>
