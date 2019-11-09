b8d5eb68d05093ac5185b696b1eaf70e64979ac4
Elton Lin <eltonlin1998@gmail.com>
2019-11-08 21:43:25 -0500

    Rename Videos to VideoGallery

--- src/views/VideoGallery.vue
+++ src/views/VideoGallery.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-    <v-app-bar
-      app
-      clipped-left
-      color="white"
-      dense
-    >
+    <!-- APP BAR -->
+    <v-app-bar app clipped-left color="white" dense>
       <v-app-bar-nav-icon @click.stop="toggleDrawer()" />
       <v-toolbar-title class="mr-12 align-center">
         <span class="title">{{ $route.params.class_id }}</span>
       </v-toolbar-title>
     </v-app-bar>
 
+    <!-- CONTENT -->
     <v-content>
       <template v-if="classDoc != {}">
         <vuetify-tabs 
@@ -19,6 +16,11 @@
           :tabs="classDoc.tabs"
           @tabs-rename="newValues => renameTabs(newValues)"
         >
+          <template v-slot:default="{ tabs }">
+            <v-tab-item v-for="(tab, i) in tabs" :key="`tab--item--${i}`"> 
+              <videos :tabNumber="i" :tabs="tabs"/>
+            </v-tab-item>
+          </template>
         </vuetify-tabs>
       </template>
     </v-content>
@@ -29,6 +31,7 @@
 import VuetifyCard from "@/components/VuetifyCard.vue"
 import BetaDoodleVideo from "@/components/BetaDoodleVideo.vue"
 import DoodleVideo from "@/views/DoodleVideo.vue"
+import Videos from "@/components/Videos.vue"
 import VuetifyTabs from "@/components/VuetifyTabs.vue"
 import db from "@/database.js"
 import firebase from "firebase/app"
@@ -40,7 +43,8 @@ export default {
     VuetifyCard,
     VuetifyTabs,
     BetaDoodleVideo,
-    DoodleVideo
+    DoodleVideo,
+    Videos
   },
   data () {
     return {
@@ -58,10 +62,7 @@ export default {
     }
   },
   created () {
-    // get all videos associated with a course 
     const classID = this.$route.params.class_id
-    // const videosRef = db.collection("whiteboards").where("fromClass", "==", classID)
-    //                                         .where("title", "==", "Sun, Lake and Mountain")
     const classRef = db.collection("classes").doc(classID)
     classRef.get().then(doc => {
       this.classDoc = doc.data()
@@ -82,11 +83,13 @@ export default {
         this.deleteVideo(videoID, audioPath)
       }
     },
-    renameTabs (newValues) {
+    async renameTabs (newValues) {
       const ref = db.collection("classes").doc(this.$route.params.class_id)
-      ref.update({
+      await ref.update({
         tabs: newValues
       })
+      const classDoc = await ref.get()
+      this.classDoc = classDoc.data()
     },
     async saveParagraph (newValue, { ".key": videoID }) {
       const ref = db.collection("whiteboards").doc(videoID)
