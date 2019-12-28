63ee6d3ed40501200f770502e6edc80dd6d0012f
Elton Lin <eltonlin1998@gmail.com>
2019-12-28 22:50:22 +0800

    Change how the title of the current class is displayed

--- src/components/BaseAppBar.vue
+++ src/components/BaseAppBar.vue
@@ -1,9 +1,17 @@
 <template>
   <v-app-bar app clipped-left color="white" dense>
-    <v-app-bar-nav-icon @click.stop="$root.$emit('toggle-drawer')"/>
-    <v-toolbar-title class="mr-12 align-center">
+    <v-app-bar-nav-icon 
+      @click.stop="$root.$emit('toggle-drawer')"
+    />
+  
+      <img src="favicon.ico" @click="$router.push('/')">
+      <v-toolbar-title class="headline font-weight-regular ml-2" @click="$router.push('/')">
+        ExplainMIT / {{ $route.params.class_id }}
+      </v-toolbar-title>
+   
+    <!-- <v-toolbar-title class="mr-12 align-center">
       <span class="title">{{ $route.params.class_id }}</span>
-    </v-toolbar-title>
+    </v-toolbar-title> -->
     <v-progress-linear
       :active="loading"
       :indeterminate="loading"
@@ -13,7 +21,7 @@
     />
     <v-spacer/>
     <slot>
-      <!-- SLOT -->
+
     </slot>
   </v-app-bar>
 </template>
