33413380779bd5ef7ae7014dbbb0a7ab5b6499c2
Elton Lin <eltonlin1998@gmail.com>
2020-01-29 18:44:54 -0500

    BaseAppBar now displays correct logo text

--- src/components/BaseAppBar.vue
+++ src/components/BaseAppBar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app-bar app clipped-left color="white" dense>
     <v-app-bar-nav-icon 
-      v-if="!this.icon" @click.stop="$root.$emit('toggle-drawer')" 
+      v-if="!icon && $route.path !== '/'" @click.stop="$root.$emit('toggle-drawer')" 
     />
     <v-icon 
       v-else-if="this.icon==='back'" 
@@ -14,7 +14,7 @@
       @click="$router.push('/')"
     >
     <v-toolbar-title class="home-logo headline font-weight-regular ml-2" @click="$router.push('/')">
-      {{ $route.path == "/" ? "ExplainMIT" : `ExplainMIT/${$route.params.class_id}` }}
+      {{ $route.path === "/" ? "ExplainMIT" : `ExplainMIT/${$route.params.class_id}` }}
     </v-toolbar-title>
     <v-progress-linear
       :active="loading"
