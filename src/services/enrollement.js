fb351f30e1e31d862a7bec02e93ebf54d33b82e4
Elton Lin <eltonlin1998@gmail.com>
2020-02-02 16:10:11 -0500

    Style changes

--- src/services/enrollement.js
+++ src/services/enrollement.js
@@ -1,54 +1,51 @@
-import db from '@/database.js'
-import {encodeKey} from '../dep'
+import db from "@/database.js";
+import { encodeKey } from "../dep";
 
 export class Enrollement {
-    constructor () {}
-    addClass (user, className) {
-        let classID = encodeKey(className)
-        let userDoc = db.collection("users").doc(user.uid)
-        userDoc.get().then(doc => {
-            // TODO: please rename A
-            let A = {...doc.data()}.enrolledClasses
-            if (!A || Array.isArray(A)) A = {};
-            if (classID in A) return;
-            var classObj = {
-                name: className,
-                settings: {
-                notifications: {
-                    newQuestion: "always",
-                  },
-                }
-            }
+  constructor () {}
 
-            A[classID] = classObj
-            userDoc.set(
-                {enrolledClasses : A},
-                {merge: true})
-        })
-    }
+  addClass (user, className) {
+    const classID = encodeKey(className);
+    const userDoc = db.collection("users").doc(user.uid);
+    userDoc.get().then(doc => {
+      // TODO: please rename A
+      let A = { ...doc.data() }.enrolledClasses;
+      if (!A || Array.isArray(A)) A = {};
+      if (classID in A) return;
+      const classObj = {
+        name: className,
+        settings: {
+          notifications: {
+            newQuestion: "always"
+          }
+        }
+      };
 
-    deleteClass(user, className) {
-        let classID = encodeKey(className)
-        let userDoc = db.collection("users").doc(user.uid)
-        userDoc.get().then(doc => {
-            let A = {...doc.data()}.enrolledClasses
+      A[classID] = classObj;
+      userDoc.set(
+        { enrolledClasses: A },
+        { merge: true }
+      );
+    });
+  }
 
-            if(!(classID in A))return
-            
-            delete A[classID]
-            
-            userDoc.set(
-                {enrolledClasses : A},
-                {merge: true})
-        })
-    }
+  deleteClass (user, className) {
+    const classID = encodeKey(className);
+    const userDoc = db.collection("users").doc(user.uid);
+    userDoc.get().then(doc => {
+      const A = { ...doc.data() }.enrolledClasses;
+      if (!(classID in A)) return;
+      delete A[classID];
+      userDoc.set({ enrolledClasses: A }, { merge: true });
+    });
+  }
 
-    changeNotification(user, className, notifType, notifFrequency) {
-        let classID = encodeKey(className)
-        let userDoc = db.collection("users").doc(user.uid)
-        const K = `enrolledClasses.${classID}.settings.notifications.${notifType}`
-        userDoc.update({
-             [K] : notifFrequency
-        })
-    }
-}   
+  changeNotification (user, className, notifType, notifFrequency) {
+    const classID = encodeKey(className);
+    const userDoc = db.collection("users").doc(user.uid);
+    const K = `enrolledClasses.${classID}.settings.notifications.${notifType}`;
+    userDoc.update({
+      [K]: notifFrequency
+    });
+  }
+}
