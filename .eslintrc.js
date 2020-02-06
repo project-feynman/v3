304c989939ea89723f681668b2c5d6fc1eb21271
Elton Lin <username@mit.edu>
2020-02-05 19:17:07 -0500

    Disable linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   'extends': [
     // 'plugin:vue/essential',
     // 'eslint:recommended',
-    '@vue/airbnb'
+    // '@vue/airbnb'
   ],
   rules: {
     "arrow-parens": ["error", "as-needed"],
