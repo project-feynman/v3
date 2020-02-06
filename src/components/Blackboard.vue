05de5b477d4bddde24ffce32a0120bb3835598b8
Elton Lin <username@mit.edu>
2020-02-05 20:47:51 -0500

    Fix videos not showing up

--- src/components/Blackboard.vue
+++ src/components/Blackboard.vue
@@ -704,33 +704,35 @@ export default {
       const whiteboardID = this.whiteboardDoc[".key"];
       const classID = this.$route.params.class_id;
 
-      // take a screenshot of the whiteboard to be used as the "preview" of the video
-      // const dataURL = this.canvas.toDataURL()
+      // Take a screenshot of the whiteboard to be used as the "preview" of the video
       const videoThumbnail = this.createThumbnail();
 
-      let metadata = {
+      const classRef = db.collection("classes").doc(classID);
+      const classDoc = await classRef.get();
+
+      const metadata = {
+        fromClass: {
+          ID: classID,
+          name: classDoc.data().name
+        },
+        creator: {
+          UID: this.user.uid,
+          name: this.user.name || "anonymous",
+          email: this.user.email
+        },
         title: videoTitle,
-        fromClass: "6.006",
         isSaved: true,
         tabNumber: 0,
         thumbnail: videoThumbnail // toDataURL takes a screenshot of a canvas and encodes it as an image URL
       };
-      if (this.user) {
-        metadata.authorUID = this.user.uid;
-        metadata.authorEmail = this.user.email;
-        if (this.user.name) {
-          metadata.authorName = this.user.name;
-        }
-      }
       if (this.currentTime) {
         metadata.duration = this.currentTime;
       }
-      db.collection("whiteboards")
-        .doc(whiteboardID)
-        .update(metadata);
+      
+      // Save on Firestore
+      db.collection("whiteboards").doc(whiteboardID).update(metadata);
 
-      // KEEP TRACK OF HOW MANY VIDEOS A CLASS HAS ACCUMULATED
-      const classRef = db.collection("classes").doc(classID);
+      // Keep track of how many videos the class has
       classRef.update({
         numOfVideos: firebase.firestore.FieldValue.increment(1)
       });
