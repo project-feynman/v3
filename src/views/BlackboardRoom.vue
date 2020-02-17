6f8a8e1a3a40926026a1323cf0a3bf4eac861e15
Elton Lin <username@mit.edu>
2020-02-17 11:02:40 -0500

    refactor: remove watchers and vue-firestore listeners

--- src/views/BlackboardRoom.vue
+++ src/views/BlackboardRoom.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="room">
-    <v-content v-if="simpleUser && room">
+    <v-content v-if="simpleUser">
       <!-- <p>room {{ room }}</p> -->
       <!-- "v-if="...room.whiteboardID"" needed because room goes from null to {} (surprisingly), before becoming fully populated -->
       <Blackboard
@@ -16,18 +16,25 @@
 <script>
 import firebase from "firebase/app";
 import "firebase/firestore";
-import db from "@/database.js";
 import Blackboard from "@/components/Blackboard.vue";
+import DatabaseHelpersMixin from "@/mixins/DatabaseHelpersMixin.js";
 
 export default {
   components: {
     Blackboard
   },
+  mixins: [DatabaseHelpersMixin],
+  data () {
+    return {
+      room: {},
+      loadCanvas: false,
+      unsubscribeRoomListener: null,
+      classId: this.$route.params.class_id,
+      roomId: this.$route.params.room_id
+    }
+  },
   computed: {
     user () { return this.$store.state.user; },
-    roomId () { return this.$route.params.room_id; },
-    classId () { return this.$route.params.class_id; },
-    roomRef () { return db.collection("rooms").doc(this.roomId); },
     simpleUser () {
       if (!this.user) return; 
       return {
@@ -37,30 +44,23 @@ export default {
       }
     }
   },
-  data () {
-    return {
-      room: null,
-      loadCanvas: false,
-      prevroomRef: null,
-    }
-  },
-  watch: {
-    $route: {
-      handler: "bindVariables",
-      immediate: true
-    }
+  async created () {
+    this.roomRef = await this.$_getDocRef(`rooms/${this.roomId}`);
+    this.bindVariables();
   },
   async beforeDestroy () {
-    this.cleanUpPrevroom(); // needed when the user switches to any other place besides another blackboard
+    this.unsubscribeRoomListener();
+    await this.roomRef.update({
+      members: firebase.firestore.FieldValue.arrayRemove(this.simpleUser)
+    });
   },
   methods: {
     async bindVariables () {
-      if (this.prevroomRef) await this.cleanUpPrevroom();
-      // Create a new room if it doesn't exist
       const room = await this.roomRef.get();
       if (!room.exists) {
         // roomId and blackboardId will be same
-        const createNewBoard = db.collection("classes").doc(this.classId).collection("blackboards").doc(this.roomId).set({})
+        const newBoardRef = await this.$_getDocRef(`classes/${this.classId}/blackboards/${this.roomId}`);
+        const createNewBoard = newBoardRef.set({})
         const createNewRoom = this.roomRef.set({
           blackboardId: this.roomId,
           members: [],
@@ -68,18 +68,9 @@ export default {
         });
         await Promise.all(createNewBoard, createNewRoom);
       }
-      await this.$binding("room", this.roomRef);
+      // TODO: remove this event listener
+      this.unsubscribeRoomListener = await this.$_dbMixin_listenToDoc(this.roomRef, this, "room");
       this.setDisconnectHook();
-      this.prevroomRef = this.roomRef;
-    },
-    async cleanUpPrevroom () {
-      const promise = new Promise(async (resolve, reject) => {
-        await this.prevroomRef.update({
-          members: firebase.firestore.FieldValue.arrayRemove(this.simpleUser)
-        });
-        resolve();
-      });
-      return promise;
     },
     setDisconnectHook () {
       const firebaseclassId = this.classId.replace(".", "-");
