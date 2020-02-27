d2765eee58643d69d105630c9aa34b419df7cc67
Elton Lin <username@mit.edu>
2020-02-27 14:49:42 -0500

    Support editing and deleting (with confirmation) explanations

--- src/components/BasePopupButton.vue
+++ src/components/BasePopupButton.vue
@@ -1,55 +1,52 @@
 <template>
-  <!-- <v-row justify="center"> -->
-    <v-dialog v-model="isOpen" persistent max-width="600px" @keydown.enter="doAction()">
-      <template v-slot:activator="{ on }">
-        <v-btn :color="color" :outlined="outlined" :disabled="disabled" v-on="on">
+  <v-dialog v-model="isOpen" persistent max-width="600px" @keydown.enter="doAction()">
+    <template v-slot:activator="{ on }">
+      <v-btn :color="color" :outlined="outlined" :disabled="disabled" v-on="on">
+        {{ actionName }}
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title><span class="headline">{{ actionName }}</span></v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <slot>
+
+            </slot>
+            <template v-for="inputField in inputFields">
+              <v-col cols="12" :key="inputField">
+                <v-text-field
+                  v-model="inputValues[inputField]"
+                  :label="inputField"
+                  :type="inputField"
+                  required
+                />
+              </v-col>
+            </template>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="resetState()" color="accent lighten-1" text>
+          Cancel
+        </v-btn>
+        <v-btn @click="doAction()" color="accent lighten-1" text>
           {{ actionName }}
         </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="headline">
-            {{ actionName }}
-          </span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <slot>
-
-              </slot>
-              <template v-for="inputField in inputFields">
-                <v-col cols="12" :key="inputField">
-                  <v-text-field
-                    v-model="inputValues[inputField]"
-                    :label="inputField"
-                    :type="inputField"
-                    required
-                  />
-                </v-col>
-              </template>
-            </v-row>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn @click="resetState()" color="accent lighten-1" text>
-            Cancel
-          </v-btn>
-          <v-btn @click="doAction()" color="accent lighten-1" text>
-            {{ actionName }}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  <!-- </v-row> -->
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
   props: {
     actionName: String,
-    inputFields: Array,
+    inputFields: {
+      type: Array,
+      default () { return []; }
+    },
     color: {
       type: String,
       default () {
--- src/components/DisplayExplanation.vue
+++ src/components/DisplayExplanation.vue
@@ -1,7 +1,11 @@
 <template>
   <v-card>
     <v-container fluid>
-      <p>{{ expl.title }}</p>
+      <p v-if="!isEditing">{{ expl.title }}</p>
+      <template v-else>
+        <v-textarea :value="expl.title" @input="newVal => editedText = newVal"/>
+        <v-btn @click="updateExplanation()" block color="accent lighten-1">SAVE EDIT</v-btn>
+      </template>
       <DoodleVideo v-if="expl.hasVisual" ref="DoodleVideo"
         :blackboardRef="boardRef" 
         :blackboardId="expl.id" 
@@ -11,21 +15,30 @@
         @click="handleVideoClick()"
         @available-resources-ready="playVideo()"
       />
+      <v-dialog v-model="isShowingPopup" max-width="600px">
+        <v-card>
+          <v-card-title>Are you sure you want to delete?</v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="isShowingPopup = false">CANCEL</v-btn>
+            <v-btn @click="deleteExplanation()">DELETE</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
       <v-row v-if="user" justify="center">
         <v-layout align-center>
           <v-spacer></v-spacer>
           <p class="pt-3">{{ hasDate? `By ${expl.creator.firstName}, ${displayDate(expl.date)}`: "" }}</p>
           <v-menu v-if="expl.creator.uid === user.uid" bottom offset-y>
             <template v-slot:activator="{ on }">
-              <v-btn v-if="expl.creator.uid === user.uid" large icon v-on="on">
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
+              <v-btn large icon v-on="on"><v-icon>mdi-dots-vertical</v-icon></v-btn>
             </template>
             <v-list>
-              <v-list-item v-for="(item, i) in ['delete']" :key="i"
-                @click="deleteExplanation()"
-              >
-                <v-list-item-title>{{ item }}</v-list-item-title>
+              <v-list-item @click="startEditing()">
+                <v-list-item-title >Edit text</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="isShowingPopup = true">
+                <v-list-item-title >Delete</v-list-item-title>
               </v-list-item>
             </v-list>
           </v-menu>
@@ -37,6 +50,7 @@
 
 <script>
 import DoodleVideo from "@/components/DoodleVideo.vue";
+import BasePopupButton from "@/components/BasePopupButton";
 import moment from "moment";
 import db from "@/database.js";
 
@@ -48,11 +62,15 @@ export default {
       default () { return true; }
     }
   },
-  components: { DoodleVideo },
+  components: { DoodleVideo, BasePopupButton },
   computed: {
     boardRef () { return db.doc(this.expl.ref); },
     user () { return this.$store.state.user; }
   },
+  data: () => ({ 
+    isEditing: false,
+    isShowingPopup: false
+  }),
   methods: {
     displayDate (date) { return moment(date).format('MMM D, h:mm a'); },
     handleVideoClick () {
@@ -61,8 +79,19 @@ export default {
     playVideo () {
       this.$refs.DoodleVideo.playGivenWhatIsAvailable(); 
     },
+    startEditing () {
+      this.editedText = this.expl.title;
+      this.isEditing = true;
+    },
+    updateExplanation () {
+      db.doc(this.expl.ref).update({
+        title: this.editedText
+      })
+      this.isEditing = false;
+    },
     deleteExplanation () {
       db.doc(this.expl.ref).delete();
+      this.isShowingPopup = false;
     }
   }
 }
