a68899ae9fa45f5e83efd01fc8c7e222215c3db5
Winston Fee <wfee@mit.edu>
2020-03-17 17:51:56 -0600

    saved rando changes

---
+++ src/pages/LiveAudio.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <LiveBoardAudio roomId="123"/>
+        <h1>
+            Hello world
+        </h1>
+    </div>
+</template>
+
+<script>
+import LiveBoardAudio from "@/components/LiveBoardAudio.vue";
+
+export default {
+    components: {
+        LiveBoardAudio
+    }
+}
+</script>
