da31707dcb16646a4b16099d122054bb4633339b
Elton Lin <username@mit.edu>
2020-03-18 16:35:12 +0800

    iPad keyboard now hides after selecting a class from the search bar, so user is not confused

--- src/components/TheSearchBar.vue
+++ src/components/TheSearchBar.vue
@@ -2,13 +2,13 @@
   <!-- return object means payload from @change will be the object
   even though the display name is just object.name -->
   <v-autocomplete
-    ref="VAutocomplete"
     :items="items"
     item-text="name"
     @change="(newClass) => handleChange(newClass)"
     return-object 
     placeholder="Search for a class to join e.g. 18.065"
     color="accent" outlined clearable elevate="2"
+    ref="VAutocomplete"
   />
 </template>
 
@@ -28,6 +28,7 @@ export default {
       const { VAutocomplete } = this.$refs;
       this.$emit("submit", newClass);
       VAutocomplete.reset();
+      VAutocomplete.blur();
     }
   }
 };
