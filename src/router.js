9540ce3b9d710ab1506a75ffc64224dc9c1e0b44
Winston Fee <wfee@mit.edu>
2020-03-19 19:46:32 -0600

    added route for testing

--- src/router.js
+++ src/router.js
@@ -12,6 +12,11 @@ export default new Router({
       component: () => import(/* webpackChunkName: "home" */ "./pages/Home.vue")
     },
     {
+      path: "/liveaudio",
+      component: () => import(/* webpackChunkName: "live-audio" */ "./pages/LiveAudio.vue")
+      },
+    {
+      
       path: "/class/:class_id",
       component: () => import(/* webpackChunkName: "class-page" */ "./pages/ClassPage.vue"),
       children: [
