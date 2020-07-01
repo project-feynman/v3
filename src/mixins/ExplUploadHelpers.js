fdf578b6691f0f2c0f8610e64fd21cd0f16b5adc
PKafle <pkafleprabhakar@gmail.com>
2020-07-01 15:25:41 -0400

    save realtime board

--- src/mixins/ExplUploadHelpers.js
+++ src/mixins/ExplUploadHelpers.js
@@ -32,7 +32,8 @@ export default {
      * @params this contains the properties `postTitle`, `strokesArray`, etc. which defines the explanation 
      * @effect uploads the explanation data to Firestore and Firebase Storage
      */
-    async $_saveExplToCacheThenUpload (thumbnailBlob, audioBlob, html, title, tags, postOrder, explRef) {
+    async $_saveExplToCacheThenUpload (thumbnailBlob, audioBlob, html, title, tags, explRef) {
+      const postOrder = (this.mitClass['maxOrder']+1) || 1;
       this.$store.commit("ADD_EXPL_TO_CACHE", {
         ref: explRef, // to uniquely identify each explanation when there are simultaneous uploads
         strokesArray: this.strokesArray,
