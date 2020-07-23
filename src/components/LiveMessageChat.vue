cb0874c63eb88207e5324c809f95c91aaf153967
Winston Fee <wfee@mit.edu>
2020-07-23 17:12:48 -0600

    got some css

--- src/components/LiveMessageChat.vue
+++ src/components/LiveMessageChat.vue
@@ -1,16 +1,22 @@
 <template>
-    <v-container>
-        <template v-for="(message,i) in sortedMessages">
-            <v-row :key="i">
-                {{message.creator.firstName}}:
-                {{message.text}} ; DATE: {{displayDate(message.date)}}
-            </v-row>
-        </template>
+    <div>
+        <div class="chat-container">
+            <div class="message" v-for="(message,index) in sortedMessages" v-bind:key="index" :class="{own: message.creator.uid == user.uid}">
+                <div class="name-display" v-if="index == 0 || sortedMessages[index-1].creator.uid != message.creator.uid">
+                    {{message.creator.firstName+ " " + message.creator.lastName}}
+                </div>
+                <div style="margin-top: 5px"></div>
+                <div class="content">
+                    <div v-html="message.content"></div>
+                    <!-- <chat-image v-if="message.image" :imgsrc="message.image" @imageLoad="imageLoad"></chat-image> -->
+                </div> 
+            </div>
+        </div>
         <v-text-field v-model="currentText">
         </v-text-field>
         <v-btn @click="addMessage()">
         </v-btn>
-    </v-container>
+    </div>
 </template>
 
 <script>
@@ -88,7 +94,7 @@ export default {
             this.messagesRef.doc(messageID).set({
                 id: messageID,
                 creator: this.simplifiedUser,
-                text: this.currentText,
+                content: this.currentText,
                 date: date
             })
         },
@@ -98,3 +104,61 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+.scrollable {
+    overflow-y: auto;
+    height: 90vh;
+  }
+  .typer{
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    bottom: 0;
+    height: 4.9rem;
+    width: 100%;
+    background-color: #fff;
+    box-shadow: 0 -5px 10px -5px rgba(0,0,0,.2);
+  }
+  .typer input[type=text]{
+    position: absolute;
+    left: 2.5rem;
+    padding: 1rem;
+    width: 80%;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    font-size: 1.25rem;
+  }
+  .chat-container{
+    box-sizing: border-box;
+    height: calc(100vh - 9.5rem);
+    overflow-y: auto;
+    padding: 10px;
+    background-color: #f2f2f2;
+  }
+  .message{
+    margin-bottom: 3px;
+  }
+  .message.own{
+    text-align: right;
+  }
+  /* .message.own .content{
+    background-color: lightskyblue;
+  } */
+  .name-display{
+    font-size: 12px;
+    font-weight: bold;
+  }
+  .chat-container .content{
+    padding: 8px;
+    /* background-color: lightgreen; */
+    /* border-style: solid; */
+    border-radius: 10px;
+    /* border-color: rgb(231, 137, 13); */
+    display:inline-block;
+    box-shadow: 0 1px 3px 0 rgba(0,0,0,0.2), 0 1px 1px 0 rgba(0,0,0,0.14), 0 2px 1px -1px rgba(0,0,0,0.12);
+    max-width: 50%;
+    /* word-wrap: break-word; */
+    }
+</style>
