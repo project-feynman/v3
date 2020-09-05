b39c9305d15a868c8e7798240633156144eb1ddb
Elton Lin <username@mit.edu>
2020-09-05 17:10:28 +0800

    Fix disconnection hook for lingering participants

--- src/components/HandleUpdatingParticipants.vue
+++ src/components/HandleUpdatingParticipants.vue
@@ -31,68 +31,44 @@ export default {
     sessionID () {
       return this.session.currentID; 
     },
-    classID () {
-      return this.$route.params.class_id;
+    participantsRef () {
+      const { class_id } = this.$route.params; 
+      return db.collection(`classes/${class_id}/participants`);
     },
-    roomID () {
-      return this.$route.params.room_id
+    myFirestoreRef () {
+      return this.participantsRef.doc(this.sessionID);
     },
-    classParticipantsRef () {
-      return db.collection(`classes/${this.classID}/participants`);
-    },
-    firebaseRef () {
-      return firebase.database().ref(`/class/${this.classID}/participants/${this.sessionID}`); 
+    myFirebaseRef () {
+      const { class_id } = this.$route.params; 
+      return firebase.database().ref(`/class/${class_id}/participants/${this.sessionID}`); 
     }
   },
-  created () {
-    // Part 1/2: Join the room 
-    firebase.database().ref(".info/connected").on("value", async isUserConnected => {
-      if (!isUserConnected.val()) {
-        return;
-      }  
-      await this.classParticipantsRef.doc(this.sessionID).set({
-        sessionID: this.sessionID,
-        currentRoom: this.$route.params.room_id,
-        roomTypeID: this.$route.params.section_id,
-        ...this.user
-      });
+  async created () {
+    // PART 1/2: set up the disconnect hook
+    const snapshot = await this.myFirebaseRef.child("disconnectCounter").once("value");
+    const disconnectCounter = snapshot.val() ? snapshot.val() : 0; 
+
+    // await Firebase to successfully register the disconnect hook 
+    // Cloud Functions will detect the change and update Firestore accordingly
+    await this.myFirebaseRef.onDisconnect().set({ 
+      disconnectCounter: disconnectCounter + 1
     });
 
-    // Part 2/2: Set a disconnect room
-    /**
-     * Firebase will remove the user object if he/she disconnects for whatever reason.
-     * 
-     * @see https://explain.mit.edu/class/mDbUrvjy4pe8Q5s5wyoD/posts/2srLvmhGXPVtmgNyNeCH
-     * @see https://firebase.google.com/docs/firestore/solutions/presence
-     * @see https://firebase.google.com/docs/database/web/offline-capabilities
-     */
-    // ".info/connected" is a special location on Firebase Realtime Database 
-    // that keeps track of whether the current client is conneceted or disconnected (see doc above)
-    firebase.database().ref(".info/connected").on("value", async snapshot => {
-      const isUserConnected = snapshot.val(); 
-      if (isUserConnected === false) {
-        return;
-      } 
-      // 1. User leaves, and his/her identity is saved to Firebase
-      // 2. Cloud Functions detects the new user in Firebase, and uses that information to `arrayRemove` the user from the room
-      await this.firebaseRef.onDisconnect().set({ uid: this.sessionID });
-
-      //user hasn't always been fetched, but uid and email are set
-      
-      this.firebaseRef.set({ // Firebase will not detect change if it's set to an empty object
-        email: "", 
-        uid: "", 
-        firstName: "" 
-      });
+    // PART 2/2: now safely join the room
+    const { room_id, section_id } = this.$route.params; 
+    this.participantsRef.doc(this.sessionID).set({
+      sessionID: this.sessionID,
+      currentRoom: room_id,
+      roomTypeID: section_id,
+      ...this.user
     });
   },
+  // CASE 2.1: Transition by switching components
   beforeDestroy () {
-    // I switched to a different component and didn't disconnect, so clean up the info/connected listener
-    firebase.database().ref(".info/connected").off();
-    this.firebaseRef.onDisconnect().cancel();
-    
-    // now delete myself from Firestore
-    this.classParticipantsRef.doc(this.sessionID).delete()
+    // disable the disconnect hook
+    this.myFirebaseRef.onDisconnect().cancel();
+    // now I can safely remove myself
+    this.myFirestoreRef.delete()
   }
 }
 </script>
