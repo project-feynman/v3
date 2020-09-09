4961c3d766cda58158dc6b0b431817536a5de614
Elton Lin <username@mit.edu>
2020-09-09 20:43:46 +0800

    Fix inconsistent variable

--- src/pages/ClassPageTemplate.vue
+++ src/pages/ClassPageTemplate.vue
@@ -69,7 +69,7 @@ export default {
       this.$store.dispatch("fetchClass", this.classID);  
     }
     db.doc(`users/${this.user.uid}`).update({
-      lastVisitedClassID: this.classID
+      mostRecentClassID: this.classID
     });
   },
   methods: {
