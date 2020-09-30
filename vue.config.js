58447e66c08880224275448a32cef008396823fa
Elton Lin <username@mit.edu>
2020-09-30 18:30:45 +0800

    Add config to deploy different versions of Explain

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
+const path = require("path");
+
 module.exports = {
   // to combat the Webpack 4.0 error (see https://stackoverflow.com/questions/57916549/vue-cli-run-build-typeerror-name-undefined)
   css: {
     sourceMap: true
-  }
+  },
+  // change these to output to different build targets
+  outputDir: path.resolve(__dirname, "dist/latest"),
 }
