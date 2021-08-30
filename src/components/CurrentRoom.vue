2615a514855afe2933beffe4315ae219c9fea2f0
Elton Lin <username@mit.edu>
2021-08-30 16:38:53 -0400

    Revert dropdown size for blackboard

--- src/components/CurrentRoom.vue
+++ src/components/CurrentRoom.vue
@@ -251,8 +251,8 @@
                        box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
                       `
                 ">
-                  <div style="margin-left: 12px">{{ getBoardNumberFromID(currentBoardID) }}</div>
-                  <v-icon class="ml-0" x-small>mdi-menu-down</v-icon>
+                  <div style="margin-left: 15px">{{ getBoardNumberFromID(currentBoardID) }}</div>
+                  <v-icon class="ml-0" small>mdi-menu-down</v-icon>
                 </span>
               </v-btn>
             </template>
