7b648b3ca3c04b75919bffac0ddf6da83c951fed
Elton Lin <username@mit.edu>
2021-09-03 11:43:02 -0400

    Make record button more visible

--- src/components/Blackboard.vue
+++ src/components/Blackboard.vue
@@ -63,11 +63,11 @@
           <template v-slot:more-actions-slot>
             <BaseButton v-if="currentState === RecordState.PRE_RECORD" 
               @click="startRecording()" 
-              icon="mdi-record" color="white" small
+              icon="mdi-record-rec" color="red" small dark
+              hasLargeIcon
             >
-              Record
             </BaseButton>
-            <BaseButton v-else @click="stopRecording()" icon="mdi-stop" color="white" small>
+            <BaseButton v-else @click="stopRecording()" icon="mdi-stop" color="red" small>
               Finish
             </BaseButton>
 
